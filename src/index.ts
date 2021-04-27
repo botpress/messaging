f40622a8302ffd83460055cafa2cb9f9777fe801
Samuel Massé <samuelmasse4@gmail.com>
2021-04-26 22:37:47 -0400

    json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
+import { createServer } from 'http'
 
+const port = process.env.PORT || '4000'
 const app = express()
-const port = 4000
+const http = createServer(app)
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('This is the messaging server!')
+  const prop = req.body.prop
+  res.send(`This is the messaging server! ${prop}`)
 })
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Server is listening on ${port}`)
 })
