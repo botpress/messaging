36b6870f84f79fa4fdd7717f7bcd1ad6a70f14ce
Samuel Massé <samuelmasse4@gmail.com>
2021-05-21 22:09:32 -0400

    fix

--- src/channels/smooch/renderers/image.ts
+++ src/channels/smooch/renderers/image.ts
@@ -1,10 +1,9 @@
 import { ImageContent } from '../../../content/types'
 import { ImageRenderer } from '../../base/renderers/image'
-import { formatUrl } from '../../url'
 import { SmoochContext } from '../context'
 
 export class SmoochImageRenderer extends ImageRenderer {
   renderImage(context: SmoochContext, payload: ImageContent): void {
-    context.messages.push({ type: 'image', mediaUrl: formatUrl(context.botUrl, payload.image) })
+    context.messages.push({ type: 'image', mediaUrl: payload.image })
   }
 }
