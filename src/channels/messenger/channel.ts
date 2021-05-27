f65124f1b5e0533339c238c86b95fa70bdd7b39a
Samuel Massé <samuelmasse4@gmail.com>
2021-05-26 20:46:09 -0400

    messenger multi instance

---
+++ src/channels/messenger/channel.ts
@@ -0,0 +1,77 @@
+import crypto from 'crypto'
+import express from 'express'
+import { Channel } from '../base/channel'
+import { MessengerInstance } from './instance'
+
+export class MessengerChannel extends Channel<MessengerInstance> {
+  get name() {
+    return 'messenger'
+  }
+
+  get id() {
+    return 'c4bb1487-b3bd-49b3-a3dd-36db908d165d'
+  }
+
+  protected createInstance(providerId: string, clientId: string): MessengerInstance {
+    return new MessengerInstance(
+      providerId,
+      clientId,
+      this.kvs,
+      this.conversations,
+      this.messages,
+      this.mapping,
+      this.loggers,
+      this.router
+    )
+  }
+
+  async setupRoutes() {
+    this.router.use(express.json({ verify: this.auth.bind(this) }))
+
+    this.router.get('/', async (req, res) => {
+      const instance = res.locals.instance as MessengerInstance
+
+      const mode = req.query['hub.mode']
+      const token = req.query['hub.verify_token']
+      const challenge = req.query['hub.challenge']
+
+      if (mode && token && mode === 'subscribe' && token === instance.config.verifyToken) {
+        this.logger.debug('Webhook Verified')
+        res.send(challenge)
+      } else {
+        res.sendStatus(403)
+      }
+    })
+
+    this.router.post('/', async (req, res) => {
+      const instance = res.locals.instance as MessengerInstance
+      const body = req.body
+
+      for (const entry of body.entry) {
+        const messages = entry.messaging
+
+        for (const webhookEvent of messages) {
+          if (!webhookEvent.sender) {
+            continue
+          }
+          await instance.receive(webhookEvent)
+        }
+      }
+
+      res.send('EVENT_RECEIVED')
+    })
+
+    this.printWebhook()
+  }
+
+  private auth(req: any, res: any, buffer: any) {
+    const instance = res.locals.instance as MessengerInstance
+
+    const signature = req.headers['x-hub-signature']
+    const [, hash] = signature.split('=')
+    const expectedHash = crypto.createHmac('sha1', instance.config.appSecret!).update(buffer).digest('hex')
+    if (hash !== expectedHash) {
+      throw new Error("Couldn't validate the request signature.")
+    }
+  }
+}
--- src/channels/messenger/instance.ts
+++ src/channels/messenger/instance.ts
@@ -1,5 +1,3 @@
-import crypto from 'crypto'
-import express from 'express'
 import { ChannelContext } from '../base/context'
 import { Instance, EndpointContent } from '../base/instance'
 import { CardToCarouselRenderer } from '../base/renderers/card'
@@ -17,41 +15,7 @@ export class MessengerInstance extends Instance<MessengerConfig, MessengerContex
   private client!: MessengerClient
 
   protected async setupConnection() {
-    this.router.use(express.json({ verify: this.auth.bind(this) }))
-
-    this.router.get('/', async (req, res) => {
-      const mode = req.query['hub.mode']
-      const token = req.query['hub.verify_token']
-      const challenge = req.query['hub.challenge']
-
-      if (mode && token && mode === 'subscribe' && token === this.config.verifyToken) {
-        this.logger.debug('Webhook Verified')
-        res.send(challenge)
-      } else {
-        res.sendStatus(403)
-      }
-    })
-
-    this.router.post('/', async (req, res) => {
-      const body = req.body
-
-      for (const entry of body.entry) {
-        const messages = entry.messaging
-
-        for (const webhookEvent of messages) {
-          if (!webhookEvent.sender) {
-            continue
-          }
-          await this.receive(webhookEvent)
-        }
-      }
-
-      res.send('EVENT_RECEIVED')
-    })
-
     this.client = new MessengerClient(this.config)
-
-    this.printWebhook()
   }
 
   protected setupRenderers() {
@@ -75,15 +39,6 @@ export class MessengerInstance extends Instance<MessengerConfig, MessengerContex
       ...base,
       client: this.client,
       messages: []
-    }
-  }
-
-  private auth(req: any, res: any, buffer: any) {
-    const signature = req.headers['x-hub-signature']
-    const [, hash] = signature.split('=')
-    const expectedHash = crypto.createHmac('sha1', this.config.appSecret!).update(buffer).digest('hex')
-    if (hash !== expectedHash) {
-      throw new Error("Couldn't validate the request signature.")
     }
   }
 }
