a856b5fa16c861c5cf3c276b05af2fb12cc2cbee
Samuel Massé <samuelmasse4@gmail.com>
2021-05-27 23:42:00 -0400

    kvs cache

--- src/kvs/service.ts
+++ src/kvs/service.ts
@@ -1,3 +1,5 @@
+import LRU from 'lru-cache'
+import ms from 'ms'
 import { v4 as uuidv4 } from 'uuid'
 import { Service } from '../base/service'
 import { DatabaseService } from '../database/service'
@@ -5,10 +7,12 @@ import { KvsTable } from './table'
 
 export class KvsService extends Service {
   private table: KvsTable
+  private cache: LRU<string, any>
 
   constructor(private db: DatabaseService) {
     super()
     this.table = new KvsTable()
+    this.cache = new LRU({ max: 10000, maxAge: ms('5min') })
   }
 
   async setup() {
@@ -16,9 +20,16 @@ export class KvsService extends Service {
   }
 
   async get(key: string): Promise<any> {
+    const cached = this.cache.get(key)
+    if (cached) {
+      return cached
+    }
+
     const rows = await this.query().where({ key })
     if (rows?.length) {
-      return this.db.getJson(rows[0].value)
+      const value = this.db.getJson(rows[0].value)
+      this.cache.set(key, value)
+      return value
     } else {
       return undefined
     }
@@ -26,8 +37,10 @@ export class KvsService extends Service {
 
   async set(key: string, value: any): Promise<void> {
     if (await this.get(key)) {
+      this.cache.set(key, value)
       await this.query().where({ key }).update({ value })
     } else {
+      this.cache.set(key, value)
       await this.query().insert({ id: uuidv4(), key, value: this.db.setJson(value) })
     }
   }
