b3f43286bfb56349747fcde76147f704c759fc73
Samuel Massé <samuelmasse4@gmail.com>
2021-06-14 11:16:57 -0400

    dotenv

--- src/config/service.ts
+++ src/config/service.ts
@@ -1,12 +1,33 @@
+import dotenv from 'dotenv'
 import fs from 'fs'
+import path from 'path'
 import { Service } from '../base/service'
 
 export class ConfigService extends Service {
   current: any
 
   async setup() {
-    // TODO: this path will change in production mode
-    const file = fs.readFileSync('res/config.json')
+    await this.setupEnv()
+    await this.setupConfig()
+  }
+
+  async setupEnv() {
+    if (process.env.NODE_ENV !== 'production') {
+      dotenv.config({ path: path.resolve(process.cwd(), 'dist', '.env') })
+    } else {
+      dotenv.config()
+    }
+  }
+
+  async setupConfig() {
+    let configPath: string
+    if (process.env.NODE_ENV !== 'production') {
+      configPath = path.resolve(process.cwd(), 'res', 'config.json')
+    } else {
+      configPath = path.resolve(process.cwd(), 'config', 'config.json')
+    }
+
+    const file = fs.readFileSync(configPath)
     this.current = JSON.parse(file.toString())
   }
 }
