201f2a8fab002795b58bbf244b04c743c7de9eb4
Samuel Massé <samuelmasse4@gmail.com>
2021-06-29 15:30:07 -0400

    remove unused apis

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,13 @@
 import express, { Router } from 'express'
 import { App } from './app'
 import { ChannelApi } from './channels/api'
-import { ClientApi } from './clients/api'
-import { ConduitApi } from './conduits/api'
 import { ConversationApi } from './conversations/api'
 import { MessageApi } from './messages/api'
-import { ProviderApi } from './providers/api'
 import { SyncApi } from './sync/api'
 
 export class Api {
   private router!: Router
 
-  providers: ProviderApi
-  conduits: ConduitApi
-  clients: ClientApi
   syncs: SyncApi
   conversations: ConversationApi
   messages: MessageApi
@@ -22,9 +16,6 @@ export class Api {
   constructor(private app: App, private root: Router) {
     this.router = Router()
 
-    this.providers = new ProviderApi(this.router, app.providers)
-    this.conduits = new ConduitApi(this.router, app.channels, app.providers, app.conduits)
-    this.clients = new ClientApi(this.router, app.clients)
     this.syncs = new SyncApi(this.router, app.syncs)
     this.conversations = new ConversationApi(this.router, app.clients, app.conversations)
     this.messages = new MessageApi(this.router, app.clients, app.channels, app.conduits, app.instances, app.messages)
@@ -48,9 +39,6 @@ export class Api {
     this.router.use(express.json())
     this.router.use(express.urlencoded({ extended: true }))
 
-    await this.providers.setup()
-    await this.conduits.setup()
-    await this.clients.setup()
     await this.syncs.setup()
     await this.conversations.setup()
     await this.messages.setup()
