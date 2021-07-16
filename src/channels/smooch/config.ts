feb70b375c713767f90fc3fb190ee690395c08d0
Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>
2021-07-16 14:26:30 -0400

    fix(smooch): add forwardRawPayloads optional config (#21)

--- src/channels/smooch/config.ts
+++ src/channels/smooch/config.ts
@@ -3,9 +3,11 @@ import Joi from 'joi'
 export interface SmoochConfig {
   keyId: string
   secret: string
+  forwardRawPayloads?: string[]
 }
 
 export const SmoochConfigSchema = Joi.object({
   keyId: Joi.string().required(),
-  secret: Joi.string().required()
+  secret: Joi.string().required(),
+  forwardRawPayloads: Joi.array().items(Joi.string()).optional()
 })
