ced5a84a1c10f47df31457816bdee2f5ab26724c
Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>
2021-07-22 11:21:37 -0400

    feat(vonage): improved typings (#36)
    * feat(vonage): improved typings
    
    * remove console
    
    Co-authored-by: Samuel Massé <samuelmasse4@gmail.com>

--- src/channels/vonage/channel.ts
+++ src/channels/vonage/channel.ts
@@ -1,5 +1,5 @@
 import crypto from 'crypto'
-import express from 'express'
+import express, { Request } from 'express'
 import jwt from 'jsonwebtoken'
 import { Channel } from '../base/channel'
 import { VonageConduit } from './conduit'
@@ -30,7 +30,7 @@ export class VonageChannel extends Channel<VonageConduit> {
       this.asyncMiddleware(async (req, res) => {
         const conduit = res.locals.conduit as VonageConduit
 
-        if (this.validate(conduit, <any>req)) {
+        if (this.validate(conduit, req)) {
           await this.app.instances.receive(conduit.conduitId, req.body)
         }
 
@@ -49,15 +49,18 @@ export class VonageChannel extends Channel<VonageConduit> {
   }
 
   private validate(conduit: VonageConduit, req: Request): boolean {
-    const body = <any>req.body
-    const [scheme, token] = (<any>req.headers).authorization.split(' ')
+    const body = req.body
+    const [scheme, token] = (req.headers.authorization || '').split(' ')
 
     if (body.from.type !== 'whatsapp' || body.to.type !== 'whatsapp' || scheme.toLowerCase() !== 'bearer' || !token) {
       return false
     }
 
     try {
-      const decoded = <any>jwt.verify(token, conduit.config.signatureSecret, { algorithms: ['HS256'] })
+      const decoded = jwt.verify(token, conduit.config.signatureSecret, { algorithms: ['HS256'] }) as {
+        api_key: string
+        payload_hash: string
+      }
 
       return (
         decoded.api_key === conduit.config.apiKey &&
