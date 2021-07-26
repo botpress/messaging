19468cf70e714015be56d80e5b12b8656709b0f8
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-07-26 11:41:11 -0400

    feat(telegram): added webhook validation

--- packages/server/src/channels/telegram/channel.ts
+++ packages/server/src/channels/telegram/channel.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express'
 import { Channel } from '../base/channel'
 import { TelegramConduit } from './conduit'
 import { TelegramConfigSchema } from './config'
@@ -25,16 +26,26 @@ export class TelegramChannel extends Channel<TelegramConduit> {
 
   async setupRoutes() {
     this.router.use(
-      '/',
+      '/:token',
       this.asyncMiddleware(async (req, res) => {
         // This is done to make forwarding work
-        req.url = '/'
+        req.url = `/${req.params.token}`
 
         const conduit = res.locals.conduit as TelegramConduit
-        conduit.callback(req, res)
+        if (this.validate(conduit, req)) {
+          conduit.callback(req, res)
+        } else {
+          this.logger.error("Request validation failed. Request probably didn't come from Telegram.")
+
+          res.status(401).send('Auth token invalid')
+        }
       })
     )
 
     this.printWebhook()
+  }
+
+  private validate(conduit: TelegramConduit, req: Request): boolean {
+    return req.params.token === conduit.config.botToken
   }
 }
