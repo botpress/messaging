d57b174052d777e69f25eca3f5656ac01fbd8e98
Samuel Massé <samuelmasse4@gmail.com>
2021-08-04 15:47:11 -0400

    fix

--- packages/server/src/health/table.ts
+++ packages/server/src/health/table.ts
@@ -8,7 +8,7 @@ export class HealthTable extends Table {
 
   create(table: Knex.CreateTableBuilder) {
     table.uuid('id').primary()
-    table.uuid('conduitId').references('id').inTable('msg_conduits').notNullable()
+    table.uuid('conduitId').references('id').inTable('msg_conduits').notNullable().onDelete('cascade')
     table.timestamp('time').notNullable()
     table.string('type').notNullable()
     table.jsonb('data').nullable()
