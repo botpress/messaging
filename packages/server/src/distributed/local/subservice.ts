c2934b6d104cfc74963099be566a44b24946b276
Samuel Massé <samuelmasse4@gmail.com>
2021-08-05 21:57:52 -0400

    local locks

--- packages/server/src/distributed/local/subservice.ts
+++ packages/server/src/distributed/local/subservice.ts
@@ -2,6 +2,8 @@ import { DistributedSubservice } from '../base/subservice'
 import { Lock } from '../types'
 
 export class LocalSubservice implements DistributedSubservice {
+  private locks: { [ressource: string]: boolean } = {}
+
   async setup() {}
 
   async destroy() {}
@@ -11,8 +13,15 @@ export class LocalSubservice implements DistributedSubservice {
   async send(channel: string, message: any) {}
 
   async lock(ressource: string): Promise<Lock> {
-    return { ressource }
+    if (!this.locks[ressource]) {
+      this.locks[ressource] = true
+      return { ressource }
+    } else {
+      throw new Error('Failed to acquire lock. Already in use.')
+    }
   }
 
-  async release(lock: Lock) {}
+  async release(lock: Lock) {
+    delete this.locks[lock.ressource]
+  }
 }
