84efd19477d096b6078032fceb09ea30d5d3e342
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-08-09 19:57:55 -0400

    fix(server): run messaging in production when using the binary

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,9 @@ import { Api } from './api'
 import { App } from './app'
 import { Launcher } from './launcher'
 
+// Set NODE_ENV to production when starting messaging using the binary version
+process.env.NODE_ENV = (<any>process).pkg ? 'production' : process.env.NODE_ENV
+
 const router = express()
 
 const app = new App()
