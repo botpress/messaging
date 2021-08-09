cfa8e4d803d3473ab866de36a14f8815af028fd4
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-08-09 16:45:44 -0400

    feat(webhook): do not send client id

--- packages/server/src/webhooks/types.ts
+++ packages/server/src/webhooks/types.ts
@@ -6,3 +6,17 @@ export interface Webhook {
   url: string
   token: string
 }
+
+export type WebhookContent =
+  | {
+      type: 'message'
+      channel: { name: string }
+      user: { id: string }
+      conversation: { id: string }
+      message: any
+    }
+  | {
+      type: 'health'
+      channel: { name: string }
+      event: any
+    }
