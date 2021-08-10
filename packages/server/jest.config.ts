4180f64d821c817a0dd78026a87d4d3ca2dd0acb
Samuel Massé <samuelmasse4@gmail.com>
2021-08-10 16:00:21 -0400

    ts jest.jconfig

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -1,9 +1,8 @@
-/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
   testMatch: ['**/test/**/(*.)test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   coverageThreshold: {
     global: {
@@ -14,3 +13,5 @@ module.exports = {
     }
   }
 }
+
+export default config
