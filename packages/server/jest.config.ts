117983aef99914bee72fbd914536e93a68b6f5be
Samuel Massé <samuelmasse4@gmail.com>
2021-08-10 16:07:47 -0400

    remove a config

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testMatch: ['**/test/**/(*.)test.ts'],
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
   clearMocks: true,
   coverageThreshold: {
     global: {
