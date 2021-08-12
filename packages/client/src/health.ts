a651ed599763a647519b4cc433407e955b26c648
Samuel Massé <samuelmasse4@gmail.com>
2021-08-12 14:46:58 -0400

    deserialize health

--- packages/client/src/health.ts
+++ packages/client/src/health.ts
@@ -2,7 +2,15 @@ import { BaseClient } from './base'
 
 export class HealthClient extends BaseClient {
   async get(): Promise<HealthReport> {
-    return (await this.http.get('/health')).data
+    return this.deserialize((await this.http.get('/health')).data)
+  }
+
+  private deserialize(report: HealthReport) {
+    for (const channel of Object.keys(report.channels)) {
+      report.channels[channel].events = report.channels[channel].events.map((x) => ({ ...x, time: new Date(x.time) }))
+    }
+
+    return report
   }
 }
 
