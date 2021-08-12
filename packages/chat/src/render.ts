c13efafc79613c9f807b3fcf29cc8a9b4dccfde2
Samuel Massé <samuelmasse4@gmail.com>
2021-08-12 01:53:53 -0400

    style and textbox

--- packages/chat/src/render.ts
+++ packages/chat/src/render.ts
@@ -19,10 +19,11 @@ export class WebchatRenderer {
     this.makeHeader()
     this.makeClientInfo()
     this.makeMessageTable()
+    this.makeTextbox()
   }
 
   private makeElement() {
-    this.element = element('div', this.parent).assignTo(this.element)
+    this.element = element('div', this.parent)
   }
 
   private makeHeader() {
@@ -34,31 +35,51 @@ export class WebchatRenderer {
   private makeClientInfo() {
     element('ul', this.element, (ul) => {
       element('li', ul, (li) => {
-        this.textClientId = text('', li).assignTo(this.textClientId)
+        this.textClientId = text('', li)
       })
       element('li', ul, (li) => {
-        this.textClientToken = text('', li).assignTo(this.textClientToken)
+        this.textClientToken = text('', li)
       })
     })
   }
 
   private makeMessageTable() {
-    element('table', this.element, (table) => {
-      element('thead', table, (thead) => {
-        element('tr', thead, (tr) => {
-          element('th', tr, (th) => {
-            text('type', th)
-          })
-          element('th', tr, (th) => {
-            text('text', th)
-          })
-          element('th', tr, (th) => {
-            text('time', th)
+    element('section', this.element, (div) => {
+      div.className = 'bp-messages-section'
+
+      element('table', div, (table) => {
+        element('thead', table, (thead) => {
+          element('tr', thead, (tr) => {
+            element('th', tr, (th) => {
+              text('type', th)
+            })
+            element('th', tr, (th) => {
+              text('text', th)
+            })
+            element('th', tr, (th) => {
+              text('time', th)
+            })
           })
         })
+        this.tbodyMessages = element('tbody', table)
       })
+    })
+  }
 
-      this.tbodyMessages = element('tbody', table).assignTo(this.tbodyMessages)
+  private makeTextbox() {
+    element('p', this.element, (p) => {
+      element('label', p, (label) => {
+        text('Type a message', label)
+      })
+      element('br', p)
+      element('input', p, (input) => {
+        input.type = 'text'
+      })
+      element('button', p, (button) => {
+        button.className = 'bp-send-button'
+
+        text('Send', button)
+      })
     })
   }
 
--- packages/chat/src/ui.ts
+++ packages/chat/src/ui.ts
@@ -3,10 +3,7 @@ export const element = <K extends keyof HTMLElementTagNameMap, N extends Node>(
   parent: N,
   construct?: (node: HTMLElementTagNameMap[K]) => void
 ) => {
-  const node = document.createElement(type) as Assignable<HTMLElementTagNameMap[K]>
-  node.assignTo = (data) => {
-    return node
-  }
+  const node = document.createElement(type)
 
   try {
     construct?.(node)
@@ -19,15 +16,7 @@ export const element = <K extends keyof HTMLElementTagNameMap, N extends Node>(
 }
 
 export const text = <N extends Node>(data: string | undefined, parent: N) => {
-  const text = document.createTextNode(data || '') as Assignable<Text>
-  text.assignTo = (data) => {
-    return text
-  }
-
+  const text = document.createTextNode(data || '')
   parent.appendChild(text)
   return text
-}
-
-type Assignable<T> = T & {
-  assignTo: (linked: T) => T
 }
