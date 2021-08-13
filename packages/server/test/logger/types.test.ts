7028ec23cc56c084cf34678eda9c79e6032a9784
Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>
2021-08-13 15:40:32 -0400

    chore(tests): refactored unit tests (#127)

--- packages/server/test/logger/types.test.ts
+++ packages/server/test/logger/types.test.ts
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import { Logger } from '../../src/logger/types'
 
 describe('Logger', () => {
+  const defaultEnv = process.env
   const scope = 'tests'
   const subScope = 'unit'
   const message = 'a message.'
@@ -12,7 +13,15 @@ describe('Logger', () => {
   }
   const levels = ['info', 'debug', 'warn', 'error']
 
-  test('Should instantiate without throwing any error', () => {
+  beforeEach(() => {
+    process.env = { ..._.cloneDeep(process.env) }
+  })
+
+  afterEach(() => {
+    process.env = defaultEnv
+  })
+
+  test('Should instantiate without throwing any error when given a scope', () => {
     try {
       const logger = new Logger(scope)
 
@@ -22,105 +31,118 @@ describe('Logger', () => {
     }
   })
 
-  test('Should allow to create sub loggers with sub scopes', () => {
-    const logger = new Logger(scope)
+  describe('sub', () => {
+    test('Should allow to create sub loggers with sub scopes', () => {
+      const logger = new Logger(scope)
 
-    const subLogger = logger.sub(subScope)
+      const subLogger = logger.sub(subScope)
 
-    expect(subLogger).toBeInstanceOf(Logger)
-    expect(subLogger['scope']).toEqual(`${scope}:${subScope}`)
+      expect(subLogger).toBeInstanceOf(Logger)
+      expect(subLogger['scope']).toEqual(`${scope}:${subScope}`)
+    })
   })
 
-  test('Should print an info message with some data', () => {
-    const logger = new Logger(scope)
-    const spy = jest.spyOn(console, 'log').mockImplementation()
+  describe(levels.join(', '), () => {
+    test('Should print a message with some data', () => {
+      const logger = new Logger(scope)
+      const spy = jest.spyOn(console, 'log').mockImplementation()
 
-    for (const level of levels) {
-      let params: any[] = [message, data]
-      let outputParams: any[] = [expect.anything(), expect.stringContaining(scope), message, data]
+      for (const level of levels) {
+        let params: any[] = [message, data]
+        let outputParams: any[] = [expect.anything(), expect.stringContaining(scope), message, data]
 
-      if (level === 'error') {
-        params.unshift(error)
-        outputParams.push(error.stack)
+        if (level === 'error') {
+          params.unshift(error)
+          outputParams.push(error.stack)
+        }
+
+        ;(<any>logger)[level](...params)
+
+        expect(spy).toHaveBeenCalledTimes(1)
+        expect(spy).toHaveBeenCalledWith(...outputParams)
+
+        spy.mockReset()
       }
+    })
 
-      ;(<any>logger)[level](...params)
+    test('Should print a message without any data', () => {
+      const logger = new Logger(scope)
+      const spy = jest.spyOn(console, 'log').mockImplementation()
+
+      for (const level of levels) {
+        let params: any[] = [message]
+        let outputParams: any[] = [expect.anything(), expect.stringContaining(scope), message]
+
+        if (level === 'error') {
+          params.unshift(error)
+          outputParams.push(error.stack)
+        }
+
+        ;(<any>logger)[level](...params)
+
+        expect(spy).toHaveBeenCalledTimes(1)
+        expect(spy).toHaveBeenCalledWith(...outputParams)
+
+        spy.mockReset()
+      }
+    })
+
+    test('Should display more info when SPINNED is true', () => {
+      process.env.SPINNED = 'true'
+
+      const logger = new Logger(scope)
+      const spy = jest.spyOn(console, 'log').mockImplementation()
+
+      for (const level of levels) {
+        let params: any[] = [message]
+        let outputParams: any[] = [expect.anything(), expect.stringContaining(`[Messaging] ${scope}`), message]
+
+        if (level === 'error') {
+          params.unshift(error)
+          outputParams.push(error.stack)
+        }
+
+        ;(<any>logger)[level](...params)
+
+        expect(spy).toHaveBeenCalledTimes(1)
+        expect(spy).toHaveBeenCalledWith(...outputParams)
+
+        spy.mockReset()
+      }
+    })
+  })
+
+  describe('error', () => {
+    test('Should print an error message and add a dot at the end', () => {
+      const logger = new Logger(scope)
+      const spy = jest.spyOn(console, 'log').mockImplementation()
+      const error = new Error()
+
+      logger.error(error, messageWithoutDot)
 
       expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(expect.anything(), expect.stringContaining(scope), message, error.stack)
+    })
 
-      expect(spy).toHaveBeenCalledWith(...outputParams)
+    test('Should print an error without a message', () => {
+      const logger = new Logger(scope)
+      const spy = jest.spyOn(console, 'log').mockImplementation()
+      const error = new Error()
 
-      spy.mockReset()
-    }
-  })
-
-  test('Should only print messages without any data', () => {
-    const logger = new Logger(scope)
-    const spy = jest.spyOn(console, 'log').mockImplementation()
-
-    for (const level of levels) {
-      let params: any[] = [message]
-      let outputParams: any[] = [expect.anything(), expect.stringContaining(scope), message]
-
-      if (level === 'error') {
-        params.unshift(error)
-        outputParams.push(error.stack)
-      }
-
-      ;(<any>logger)[level](...params)
+      logger.error(error)
 
       expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(expect.anything(), expect.stringContaining(scope), error.stack)
+    })
 
-      expect(spy).toHaveBeenCalledWith(...outputParams)
+    test('Should print an error message without a stack trace', () => {
+      const logger = new Logger(scope)
+      const spy = jest.spyOn(console, 'log').mockImplementation()
 
-      spy.mockReset()
-    }
-  })
+      logger.error(undefined, message)
 
-  test('Should print an error message and add a dot at the end', () => {
-    const logger = new Logger(scope)
-    const spy = jest.spyOn(console, 'log').mockImplementation()
-    const error = new Error()
-
-    logger.error(error, messageWithoutDot)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(expect.anything(), expect.stringContaining(scope), message, error.stack)
-  })
-
-  test('Should print an error without a message', () => {
-    const logger = new Logger(scope)
-    const spy = jest.spyOn(console, 'log').mockImplementation()
-    const error = new Error()
-
-    logger.error(error)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(expect.anything(), expect.stringContaining(scope), error.stack)
-  })
-
-  test('Should print an error message without a stack trace', () => {
-    const logger = new Logger(scope)
-    const spy = jest.spyOn(console, 'log').mockImplementation()
-
-    logger.error(undefined, message)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(expect.anything(), expect.stringContaining(scope), message)
-  })
-
-  test('Spinned env var should display more info', () => {
-    const env = _.cloneDeep(process.env)
-    process.env.SPINNED = 'true'
-
-    const logger = new Logger(scope)
-    const spy = jest.spyOn(console, 'log').mockImplementation()
-
-    logger.info(message, data)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(expect.anything(), expect.stringContaining(`[Messaging] ${scope}`), message, data)
-
-    process.env = env
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(expect.anything(), expect.stringContaining(scope), message)
+    })
   })
 })
--- packages/server/test/post/service.test.ts
+++ packages/server/test/post/service.test.ts
@@ -9,6 +9,7 @@ jest.mock('../../src/logger/types')
 jest.mock('axios')
 
 describe('PostService', () => {
+  const defaultEnv = process.env
   let configService: ConfigService
   let data: any
   let axiosConfig: AxiosRequestConfig
@@ -18,6 +19,8 @@ describe('PostService', () => {
   const password = 'asuperpassword'
 
   beforeEach(async () => {
+    process.env = { ..._.cloneDeep(process.env) }
+
     configService = new ConfigService()
     configService['current'] = {}
 
@@ -26,7 +29,11 @@ describe('PostService', () => {
     headers = { custom: 'header' }
   })
 
-  test('Should instantiate without throwing any error', async () => {
+  afterEach(() => {
+    process.env = defaultEnv
+  })
+
+  test('Should not throw any error with a default configuration', async () => {
     try {
       const postService = new PostService(configService)
       await postService.setup()
@@ -39,98 +46,100 @@ describe('PostService', () => {
     }
   })
 
-  test('Should call a URL making a post request', async () => {
-    data = undefined
+  describe('send', () => {
+    test('Should call a URL making a post request', async () => {
+      data = undefined
 
-    const spy = jest.spyOn(axios, 'post')
+      const spy = jest.spyOn(axios, 'post')
 
-    const postService = new PostService(configService)
-    await postService.setup()
+      const postService = new PostService(configService)
+      await postService.setup()
 
-    await postService.send(url, data)
+      await postService.send(url, data)
 
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
-  })
-
-  test('Should send data to a URL making a post request', async () => {
-    const spy = jest.spyOn(axios, 'post')
-
-    const postService = new PostService(configService)
-    await postService.setup()
-
-    await postService.send(url, data)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
-  })
-
-  test('Should send data to a URL making a post request using custom headers', async () => {
-    axiosConfig.headers = headers
-
-    const spy = jest.spyOn(axios, 'post')
-
-    const postService = new PostService(configService)
-    await postService.setup()
-
-    await postService.send(url, data, headers)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
-  })
-
-  test('Should send data to a URL making a post request using custom headers and a password from env var', async () => {
-    const env = _.cloneDeep(process.env)
-    process.env.INTERNAL_PASSWORD = password
-
-    axiosConfig.headers = { ...headers, password }
-
-    const spy = jest.spyOn(axios, 'post')
-
-    const postService = new PostService(configService)
-    await postService.setup()
-
-    await postService.send(url, data, headers)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
-
-    process.env = env
-  })
-
-  test('Should send data to a URL making a post request using custom headers and a password from config file', async () => {
-    configService['current'].security = { password }
-    axiosConfig.headers = { ...headers, password }
-
-    const spy = jest.spyOn(axios, 'post')
-
-    const postService = new PostService(configService)
-    await postService.setup()
-
-    await postService.send(url, data, headers)
-
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
-  })
-
-  test('Should log the error if the request fails', async () => {
-    const error = new Error('error')
-    const spy = jest.spyOn(axios, 'post').mockImplementationOnce(() => Promise.reject(error))
-
-    const postService = new PostService(configService)
-    await postService.setup()
-    Object.defineProperty(postService, 'attempts', {
-      value: 1,
-      writable: false
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
     })
 
-    const loggerSpy = jest.spyOn(postService['logger'], 'error')
+    test('Should send data to a URL making a post request', async () => {
+      const spy = jest.spyOn(axios, 'post')
 
-    await postService.send(url, data)
+      const postService = new PostService(configService)
+      await postService.setup()
 
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
-    expect(loggerSpy).toBeCalledTimes(1)
-    expect(loggerSpy).toBeCalledWith(error, expect.anything())
+      await postService.send(url, data)
+
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
+    })
+
+    test('Should send data to a URL making a post request and using custom headers', async () => {
+      axiosConfig.headers = headers
+
+      const spy = jest.spyOn(axios, 'post')
+
+      const postService = new PostService(configService)
+      await postService.setup()
+
+      await postService.send(url, data, headers)
+
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
+    })
+
+    test('Should send data to a URL making a post request, using custom headers and a password from an env var', async () => {
+      const env = _.cloneDeep(process.env)
+      process.env.INTERNAL_PASSWORD = password
+
+      axiosConfig.headers = { ...headers, password }
+
+      const spy = jest.spyOn(axios, 'post')
+
+      const postService = new PostService(configService)
+      await postService.setup()
+
+      await postService.send(url, data, headers)
+
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
+
+      process.env = env
+    })
+
+    test('Should send data to a URL making a post request, using custom headers and a password from a config file', async () => {
+      configService['current'].security = { password }
+      axiosConfig.headers = { ...headers, password }
+
+      const spy = jest.spyOn(axios, 'post')
+
+      const postService = new PostService(configService)
+      await postService.setup()
+
+      await postService.send(url, data, headers)
+
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
+    })
+
+    test('Should log the error if the request fails', async () => {
+      const error = new Error('error')
+      const spy = jest.spyOn(axios, 'post').mockImplementationOnce(() => Promise.reject(error))
+
+      const postService = new PostService(configService)
+      await postService.setup()
+      Object.defineProperty(postService, 'attempts', {
+        value: 1,
+        writable: false
+      })
+
+      const loggerSpy = jest.spyOn(postService['logger'], 'error')
+
+      await postService.send(url, data)
+
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith(url, data, axiosConfig)
+      expect(loggerSpy).toBeCalledTimes(1)
+      expect(loggerSpy).toBeCalledWith(error, expect.anything())
+    })
   })
 })
