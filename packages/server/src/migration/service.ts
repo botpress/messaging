f97df2f14d34267b0e86bf7746a552e66b532a57
Samuel Massé <samuelmasse4@gmail.com>
2021-08-23 21:03:36 -0400

    refact

--- packages/server/src/migration/service.ts
+++ packages/server/src/migration/service.ts
@@ -1,4 +1,5 @@
 import clc from 'cli-color'
+import { Knex } from 'knex'
 import _ from 'lodash'
 import semver from 'semver'
 import yn from 'yn'
@@ -27,88 +28,52 @@ export class MigrationService extends Service {
     this.isDown = process.env.MIGRATE_CMD === 'down'
     this.isDry = !!yn(process.env.MIGRATE_DRYRUN)
 
-    const allMigrations = this.listAllMigrations()
-
-    const migrationsToRun = this.isDown
-      ? allMigrations
-          .filter((x) => semver.lte(x.meta.version, this.srcVersion) && semver.gt(x.meta.version, this.dstVersion))
-          .reverse()
-      : allMigrations.filter(
-          (x) => semver.gt(x.meta.version, this.srcVersion) && semver.lte(x.meta.version, this.dstVersion)
-        )
-
-    await this.migrate(migrationsToRun)
+    await this.migrate()
 
     if (process.env.MIGRATE_CMD) {
       throw new ShutDownSignal()
     }
   }
 
-  private async migrate(migrationsToRun: Migration[]) {
-    if (migrationsToRun.length || this.isDry) {
-      this.logger.window(
-        [
-          clc.bold(this.isDry ? 'DRY RUN' : 'Migrations Required'),
-          clc.blackBright(`Version ${this.srcVersion} => ${this.dstVersion}`),
-          clc.blackBright(`${migrationsToRun.length} changes`)
-        ],
-        LoggerLevel.Warn
-      )
-
-      const migrationsByVersion = _.groupBy(migrationsToRun, 'meta.version')
-
-      for (const [version, migrations] of Object.entries(migrationsByVersion)) {
-        this.logger.warn(clc.bold(version))
-        for (const migration of migrations) {
-          this.logger.warn(`- ${this.isDown ? '[rollback] ' : ''}${migration.meta.description}`)
-        }
-      }
-
-      if (this.isDry) {
-        await this.runMigrations(migrationsToRun)
-        throw new ShutDownSignal()
-      }
-
-      if (!yn(process.env.AUTO_MIGRATE)) {
-        this.logger.error(undefined, 'Migrations required. Please restart the server with AUTO_MIGRATE=true')
-        throw new ShutDownSignal()
-      }
-
-      await this.runMigrations(migrationsToRun)
+  private async migrate() {
+    const migrations = this.listMigrationsToRun()
+    if (!migrations.length && !this.isDry) {
+      return this.updateDbVersion()
     }
 
-    if (!this.isDry && !semver.eq(this.meta.get().version, this.dstVersion)) {
-      await this.meta.update({ version: this.dstVersion })
+    this.showMigrationsRequiredWindow(migrations.length)
+    this.showMigrationPlan(migrations)
+
+    if (this.isDry) {
+      await this.runMigrations(migrations)
+      throw new ShutDownSignal()
     }
+
+    if (!yn(process.env.AUTO_MIGRATE)) {
+      this.logger.error(undefined, 'Migrations required. Please restart the server with AUTO_MIGRATE=true')
+      throw new ShutDownSignal()
+    }
+
+    await this.runMigrations(migrations)
+    await this.updateDbVersion()
   }
 
-  private async runMigrations(migrationsToRun: Migration[]) {
+  private async runMigrations(migrations: Migration[]) {
     const logPrefix = this.isDry ? clc.blackBright('[DRY] ') : ''
+    const log = (text: string) => {
+      this.logger.info(`${logPrefix}${text}`)
+    }
+
     this.logger.window([
-      clc.bold(`${logPrefix}Executing ${migrationsToRun.length} migration${migrationsToRun.length > 1 ? 's' : ''}`)
+      clc.bold(`${logPrefix}Executing ${migrations.length} migration${migrations.length > 1 ? 's' : ''}`)
     ])
 
-    const migrationsByVersion = _.groupBy(migrationsToRun, 'meta.version')
+    const migrationsByVersion = _.groupBy(migrations, 'meta.version')
     const trx = await this.db.knex.transaction()
 
     try {
       for (const [version, migrations] of Object.entries(migrationsByVersion)) {
-        this.logger.info(`${logPrefix}${clc.bold(version)}`)
-
-        for (const migration of migrations) {
-          this.logger.info(`${logPrefix}Running`, migration.meta.name)
-          migration.transact(trx)
-          if ((await migration.applied(this.isDown)) === this.isDown) {
-            if (this.isDown) {
-              await migration.down()
-            } else {
-              await migration.up()
-            }
-            this.logger.info(`${logPrefix}- Success`)
-          } else {
-            this.logger.info(`${logPrefix}- Skipped`)
-          }
-        }
+        await this.runMigrationsForVersion(version, migrations, trx, log)
       }
 
       if (this.isDry) {
@@ -117,7 +82,7 @@ export class MigrationService extends Service {
         await trx.commit()
       }
 
-      this.logger.info(`${logPrefix}Migrations completed successfully!`)
+      log('Migrations completed successfully!')
     } catch (e) {
       await trx.rollback()
       this.logger.error(e, `${logPrefix}Migrations failed`)
@@ -125,12 +90,51 @@ export class MigrationService extends Service {
     }
   }
 
+  private async runMigrationsForVersion(
+    version: string,
+    migrations: Migration[],
+    trx: Knex.Transaction,
+    log: (text: string) => void
+  ) {
+    log(clc.bold(version))
+
+    for (const migration of migrations) {
+      log(`Running ${migration.meta.name}`)
+
+      migration.transact(trx)
+
+      if ((await migration.applied(this.isDown)) === this.isDown) {
+        if (this.isDown) {
+          await migration.down()
+        } else {
+          await migration.up()
+        }
+
+        log('- Success')
+      } else {
+        log('- Skipped')
+      }
+    }
+  }
+
+  private listMigrationsToRun() {
+    const allMigrations = this.listAllMigrations()
+
+    return this.isDown
+      ? allMigrations
+          .filter((x) => semver.lte(x.meta.version, this.srcVersion) && semver.gt(x.meta.version, this.dstVersion))
+          .reverse()
+      : allMigrations.filter(
+          (x) => semver.gt(x.meta.version, this.srcVersion) && semver.lte(x.meta.version, this.dstVersion)
+        )
+  }
+
   private listAllMigrations() {
     const all = migs.map((x) => new x()) as Migration[]
     const alphabetical = all.sort((a, b) => {
       return a.meta.name.localeCompare(b.meta.name, 'en')
     })
-    const versions = alphabetical.sort((a, b) => {
+    return alphabetical.sort((a, b) => {
       if (semver.gt(a.meta.version, b.meta.version)) {
         return 1
       } else if (semver.lt(a.meta.version, b.meta.version)) {
@@ -139,7 +143,33 @@ export class MigrationService extends Service {
         return 0
       }
     })
+  }
 
-    return versions
+  private async updateDbVersion() {
+    if (!this.isDry && !semver.eq(this.meta.get().version, this.dstVersion)) {
+      await this.meta.update({ version: this.dstVersion })
+    }
+  }
+
+  private showMigrationsRequiredWindow(migrationCount: number) {
+    this.logger.window(
+      [
+        clc.bold(this.isDry ? 'DRY RUN' : 'Migrations Required'),
+        clc.blackBright(`Version ${this.srcVersion} => ${this.dstVersion}`),
+        clc.blackBright(`${migrationCount} changes`)
+      ],
+      LoggerLevel.Warn
+    )
+  }
+
+  private showMigrationPlan(migrationsToRun: Migration[]) {
+    const migrationsByVersion = _.groupBy(migrationsToRun, 'meta.version')
+
+    for (const [version, migrations] of Object.entries(migrationsByVersion)) {
+      this.logger.warn(clc.bold(version))
+      for (const migration of migrations) {
+        this.logger.warn(`- ${this.isDown ? '[rollback] ' : ''}${migration.meta.description}`)
+      }
+    }
   }
 }
