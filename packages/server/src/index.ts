c0b39c365f1f6095ecc89cbe1a9547073ac6b2a8
Samuel Massé <samuelmasse4@gmail.com>
2021-08-23 22:18:14 -0400

    yargs

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,6 @@
 import './rewire'
 import express from 'express'
+import yargs from 'yargs'
 import { Api } from './api'
 import { App } from './app'
 import { Launcher } from './launcher'
@@ -7,10 +8,17 @@ import { Launcher } from './launcher'
 // Set NODE_ENV to production when starting messaging using the binary version
 process.env.NODE_ENV = (<any>process).pkg ? 'production' : process.env.NODE_ENV
 
-const router = express()
+void yargs.command(
+  '$0',
+  'Start server',
+  () => {},
+  async () => {
+    const router = express()
 
-const app = new App()
-const api = new Api(app, router)
+    const app = new App()
+    const api = new Api(app, router)
 
-const launcher = new Launcher(router, app, api)
-void launcher.launch()
+    const launcher = new Launcher(router, app, api)
+    await launcher.launch()
+  }
+).argv
