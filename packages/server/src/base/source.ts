3f1f38de422e5bdfc4af0a3fc58c1481d1c383f1
Samuel Massé <samuelmasse4@gmail.com>
2021-09-07 18:43:59 -0400

    better doc

--- packages/server/src/base/source.ts
+++ packages/server/src/base/source.ts
@@ -1,6 +1,15 @@
 import { uuid } from '@botpress/messaging-base'
 import { Endpoint } from '../mapping/types'
 
+/**
+ * Indicates whether an action is performed due to a request made by
+ * - a conduit (we receive a request from an external service such as Telegram)
+ * - a client (using the http api with a clientId and clientToken)
+ * - a socket (using a websocket connection)
+ *
+ * Knowing this allows messaging to avoid redundantly
+ * streaming back an event to the service that made the request.
+ */
 export interface ActionSource {
   conduit?: { id: uuid; endpoint: Endpoint }
   client?: { id: uuid }
