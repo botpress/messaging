b28db16413766c6f5c846161e4a43caea7929813
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-10-14 11:28:35 -0400

    small refactor

--- packages/client/src/base.ts
+++ packages/client/src/base.ts
@@ -1,5 +1,26 @@
-import { AxiosInstance } from 'axios'
-import { handleError } from './errors'
+import axios, { AxiosInstance } from 'axios'
+import { BadRequestError, UnauthorizedError, ForbiddenError, InternalServerError } from './errors'
+
+const handleError = (err: unknown) => {
+  if (axios.isAxiosError(err)) {
+    switch (err.response?.status) {
+      case 400:
+        throw new BadRequestError(err.message)
+      case 401:
+        throw new UnauthorizedError(err.message)
+      case 403:
+        throw new ForbiddenError(err.message)
+      case 404:
+        return undefined
+      case 500:
+        throw new InternalServerError(err.message)
+      default:
+        throw err
+    }
+  }
+
+  throw err
+}
 
 export abstract class BaseClient {
   constructor(protected http: AxiosInstance) {
@@ -7,7 +28,7 @@ export abstract class BaseClient {
       (response) => {
         return response
       },
-      (error: unknown) => {
+      (error) => {
         return handleError(error)
       }
     )
--- packages/client/src/errors.ts
+++ packages/client/src/errors.ts
@@ -1,5 +1,3 @@
-import axios from 'axios'
-
 export class BadRequestError extends Error {
   static statusCode = 400
 }
@@ -14,25 +12,4 @@ export class ForbiddenError extends Error {
 
 export class InternalServerError extends Error {
   static statusCode = 500
-}
-
-export const handleError = (err: unknown) => {
-  if (axios.isAxiosError(err)) {
-    switch (err.response?.status) {
-      case 400:
-        throw new BadRequestError(err.message)
-      case 401:
-        throw new UnauthorizedError(err.message)
-      case 403:
-        throw new ForbiddenError(err.message)
-      case 404:
-        return undefined
-      case 500:
-        throw new InternalServerError(err.message)
-      default:
-        throw err
-    }
-  }
-
-  throw err
 }
