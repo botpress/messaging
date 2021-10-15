f864d62baa83c5b2d466f9058359f8d04fec555f
Samuel Massé <samuelmasse4@gmail.com>
2021-10-15 17:02:53 -0400

    refact function

--- packages/server/src/base/auth/client.ts
+++ packages/server/src/base/auth/client.ts
@@ -11,26 +11,21 @@ export class ClientAuthHandler extends AuthHandler {
 
   public auth(fn: Middleware<ClientApiRequest>) {
     return this.asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const clientId = req.headers['x-bp-messaging-client-id'] as string
-        const clientToken = req.headers['x-bp-messaging-client-token'] as string
+      const clientId = req.headers['x-bp-messaging-client-id'] as string
+      const clientToken = req.headers['x-bp-messaging-client-token'] as string
 
-        if (!validateUuid(clientId)) {
-          return res.sendStatus(403)
-        }
+      if (!validateUuid(clientId)) {
+        return res.sendStatus(401)
+      }
 
-        const client = await this.clients.getByIdAndToken(clientId, clientToken)
-
-        if (!client) {
-          return res.sendStatus(403)
-        } else {
-          const nreq = req as ClientApiRequest
-          nreq.client = client
-          await fn(nreq, res, next)
-        }
-      } catch {
+      const client = await this.clients.getByIdAndToken(clientId, clientToken)
+      if (!client) {
         return res.sendStatus(403)
       }
+
+      const nreq = req as ClientApiRequest
+      nreq.client = client
+      return fn(nreq, res, next)
     })
   }
 }
