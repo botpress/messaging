744182315f44d8211de3ff13654962d3d19a855e
Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>
2021-10-15 12:17:52 -0400

    feat(api): add a route to get user info (#195)
    * feat(api): add a route to get user info
    
    * pr comment
    
    * chore(api): better handling of entity not found (#197)
    
    * chore(api): better handling of entity not found
    
    * chore(client): better error handling (#198)
    
    * chore(client): better error handling
    
    * small refactor

--- packages/client/src/base.ts
+++ packages/client/src/base.ts
@@ -1,5 +1,36 @@
-import { AxiosInstance } from 'axios'
+import axios, { AxiosInstance } from 'axios'
+import { BadRequestError, UnauthorizedError, ForbiddenError, InternalServerError } from './errors'
+
+const handleError = (err: unknown) => {
+  if (axios.isAxiosError(err)) {
+    switch (err.response?.status) {
+      case 400:
+        throw new BadRequestError(err.message)
+      case 401:
+        throw new UnauthorizedError(err.message)
+      case 403:
+        throw new ForbiddenError(err.message)
+      case 404:
+        return undefined
+      case 500:
+        throw new InternalServerError(err.message)
+      default:
+        throw err
+    }
+  }
+
+  throw err
+}
 
 export abstract class BaseClient {
-  constructor(protected http: AxiosInstance) {}
+  constructor(protected http: AxiosInstance) {
+    http.interceptors.response.use(
+      (response) => {
+        return response
+      },
+      (error) => {
+        return handleError(error)
+      }
+    )
+  }
 }
