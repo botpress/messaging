6591ab73170afc56c9d6d4b5c6af7f7519ff3f3f
Samuel Massé <samuelmasse4@gmail.com>
2021-10-15 17:39:29 -0400

    refact

---
+++ packages/server/src/base/auth/client.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express'
+import { validate as validateUuid } from 'uuid'
+import { ClientService } from '../../clients/service'
+import { Client } from '../../clients/types'
+import { AuthHandler, Middleware } from './base'
+
+export class ClientAuthHandler extends AuthHandler {
+  constructor(private clients: ClientService) {
+    super()
+  }
+
+  public auth(fn: Middleware<ClientApiRequest>) {
+    return this.asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
+      const clientId = req.headers['x-bp-messaging-client-id'] as string
+      const clientToken = req.headers['x-bp-messaging-client-token'] as string
+
+      if (!validateUuid(clientId)) {
+        return res.sendStatus(401)
+      }
+
+      const client = await this.clients.getByIdAndToken(clientId, clientToken)
+      if (!client) {
+        return res.sendStatus(401)
+      }
+
+      const clientApiReq = req as ClientApiRequest
+      clientApiReq.client = client
+      return fn(clientApiReq, res, next)
+    })
+  }
+}
+
+export type ClientApiRequest = Request & {
+  client: Client
+}
