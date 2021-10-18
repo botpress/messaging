113d0f229cc85b003c78f3b6dfd98116224b104f
Charles Catta <charles.catta@botpress.com>
2021-10-18 14:03:02 -0400

    Remove useless babel

--- packages/react-message-renderer/babel.config.js
+++
@@ -1,5 +0,0 @@
-// babel.config.js
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
-  plugins: ['@babel/plugin-transform-typescript', '@babel/plugin-proposal-class-properties']
-};
