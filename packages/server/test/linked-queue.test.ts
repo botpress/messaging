063615b19509573fdfa7c8340da2a460ca6d8fb6
Samuel Massé <samuelmasse4@gmail.com>
2021-10-18 17:36:16 -0400

    more tests

--- packages/server/test/linked-queue.test.ts
+++ packages/server/test/linked-queue.test.ts
@@ -1,13 +1,10 @@
 import { LinkedQueue } from '../src/instances/queue'
 
+const QUEUE_COUNT = 20
+const QUEUE_VALUE_OFFSET = 100
+
 describe('Linked Queue', () => {
   const state: { queue?: LinkedQueue<number>; value?: number } = {}
-
-  beforeAll(async () => {})
-
-  afterAll(async () => {})
-
-  beforeEach(async () => {})
 
   test('Create linked queue', async () => {
     const queue = new LinkedQueue<number>()
@@ -29,11 +26,10 @@ describe('Linked Queue', () => {
     }).toThrow()
   })
 
-  test('Equeue and peek', async () => {
+  test('Equeue', async () => {
     state.value = 14
     state.queue!.enqueue(state.value)
     expect(state.queue!.count).toBe(1)
-    expect(state.queue!.peek()).toBe(state.value)
   })
 
   test('Peek should return correct value', async () => {
@@ -46,5 +42,41 @@ describe('Linked Queue', () => {
     const value = state.queue!.dequeue()
     expect(value).toBe(state.value)
     expect(state.queue!.count).toBe(0)
+  })
+
+  test('Equeue many', async () => {
+    for (let i = 0; i < QUEUE_COUNT; i++) {
+      state.queue?.enqueue(i + QUEUE_VALUE_OFFSET)
+      expect(state.queue!.count).toBe(i + 1)
+    }
+
+    expect(state.queue!.count).toBe(20)
+  })
+
+  test('Dequeue many', async () => {
+    for (let i = 0; i < QUEUE_COUNT; i++) {
+      const value = state.queue?.dequeue()
+      expect(value).toBe(i + QUEUE_VALUE_OFFSET)
+      expect(state.queue!.count).toBe(QUEUE_COUNT - i - 1)
+    }
+
+    expect(state.queue!.count).toBe(0)
+  })
+
+  test('Equeue many and dequeue half', async () => {
+    for (let i = 0; i < QUEUE_COUNT; i++) {
+      state.queue?.enqueue(i + QUEUE_VALUE_OFFSET)
+      expect(state.queue!.count).toBe(i + 1)
+    }
+
+    expect(state.queue!.count).toBe(20)
+
+    for (let i = 0; i < QUEUE_COUNT / 2; i++) {
+      const value = state.queue?.dequeue()
+      expect(value).toBe(i + QUEUE_VALUE_OFFSET)
+      expect(state.queue!.count).toBe(QUEUE_COUNT - i - 1)
+    }
+
+    expect(state.queue!.count).toBe(QUEUE_COUNT / 2)
   })
 })
