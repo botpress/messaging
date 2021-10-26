8d208f82a721e103c038a8a7ffa88118080f2587
Samuel Massé <samuelmasse4@gmail.com>
2021-10-25 22:01:33 -0400

    fix

--- packages/socket/src/index.ts
+++ packages/socket/src/index.ts
@@ -1,7 +1,4 @@
 export * from '@botpress/messaging-base'
 export * from './base'
 export * from './com'
-export * from './conversations'
-export * from './messages'
 export * from './socket'
-export * from './users'
