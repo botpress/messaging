0886cf884cdb4528e3230cc67e941550bc74d04b
Samuel Massé <samuelmasse4@gmail.com>
2021-10-26 20:42:27 -0400

    more tests

--- packages/socket/test/socket.test.ts
+++ packages/socket/test/socket.test.ts
@@ -1,4 +1,4 @@
-import { MessagingSocket } from '../src'
+import { Conversation, Message, MessagingSocket } from '../src'
 
 // TODO: improve this test to be more automated. Right now it requires starting
 // a messaging server on port 3100 and pasting an existing client id in the CLIENT_ID variable.
@@ -6,7 +6,13 @@ import { MessagingSocket } from '../src'
 const CLIENT_ID = '475dc210-0beb-4696-815f-2441b745fa4c'
 
 describe('Socket Client', () => {
-  const state: { socket?: MessagingSocket } = {}
+  const state: {
+    socket?: MessagingSocket
+    userId?: string
+    conversation?: Conversation
+    message1?: Message
+    message2?: Message
+  } = {}
 
   test('Connect', async () => {
     const socket = new MessagingSocket({
@@ -38,6 +44,7 @@ describe('Socket Client', () => {
       }, 2500)
 
       state.socket!.on('login', (creds) => {
+        state.userId = creds.userId
         clearTimeout(timeout)
         resolve()
       })
@@ -45,6 +52,36 @@ describe('Socket Client', () => {
 
     await state.socket!.login()
     await promise
+  })
+
+  test('CreateConversation', async () => {
+    const conversation = await state.socket?.createConversation()
+
+    expect(conversation?.clientId).toBe(CLIENT_ID)
+    expect(conversation?.userId).toBe(state.userId)
+
+    state.conversation = conversation
+  })
+
+  test('SendText', async () => {
+    const message = await state.socket?.sendText('Hello!')
+
+    expect(message?.authorId).toBe(state.userId)
+    expect(message?.conversationId).toBe(state.conversation?.id)
+    expect(message?.payload.text).toEqual('Hello!')
+
+    state.message1 = message!
+  })
+
+  test('SendPayload', async () => {
+    const payload = { type: 'image', image: 'blabla.jpg' }
+    const message = await state.socket?.sendPayload(payload)
+
+    expect(message?.authorId).toBe(state.userId)
+    expect(message?.conversationId).toBe(state.conversation?.id)
+    expect(message?.payload).toEqual(payload)
+
+    state.message2 = message!
   })
 
   test('Disconnect', async () => {
