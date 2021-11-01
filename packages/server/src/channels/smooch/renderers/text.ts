6e50b72b83c723b5d6a3325bad335e10fb778485
Charles Catta <charles.catta@botpress.com>
2021-11-01 13:57:41 -0400

    fix(smooch): strip markdown from messages

--- packages/server/src/channels/smooch/renderers/text.ts
+++ packages/server/src/channels/smooch/renderers/text.ts
@@ -1,9 +1,16 @@
+import { remark } from 'remark'
+import strip from 'strip-markdown'
 import { TextContent } from '../../../content/types'
 import { TextRenderer } from '../../base/renderers/text'
 import { SmoochContext } from '../context'
 
 export class SmoochTextRenderer extends TextRenderer {
   renderText(context: SmoochContext, payload: TextContent): void {
+    if (payload.markdown) {
+      const cleanText = remark().use(strip).processSync(payload.text)
+      payload.text = String(cleanText)
+    }
+
     context.messages.push({ type: 'text', text: payload.text })
   }
 }
