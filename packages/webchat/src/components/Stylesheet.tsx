e9ea448244a7963a1d5b05de2cf441e65ea541e3
Samuel Massé <samuelmasse4@gmail.com>
2021-11-10 14:54:11 -0500

    pr comments

--- packages/webchat/src/components/Stylesheet.tsx
+++ packages/webchat/src/components/Stylesheet.tsx
@@ -1,3 +1,3 @@
 import React from 'react'
 
-export default ({ href }: { href: any }) => <link rel="stylesheet" type="text/css" href={href} />
+export default ({ href }: { href: string }) => <link rel="stylesheet" type="text/css" href={href} />
