1edb476dabf1b44ef2c85c21d97d9af6f803c1f0
Samuel Massé <59894025+samuelmasse@users.noreply.github.com>
2021-11-12 17:33:29 -0500

    feat(webchat): port webchat injection (#239)
    * user token start
    
    * tests
    
    * refactor auth
    
    * caching
    
    * api
    
    * fix test
    
    * comment
    
    * expiry
    
    * batching
    
    * remove user token implementation
    
    * Revert "remove user token implementation"
    
    This reverts commit 1e1dd7807671675231b9ac09b244928ede4dc179.
    
    * refact auth
    
    * remove file
    
    * fix
    
    * refact client auth function
    
    * refact function
    
    * refact
    
    * fix
    
    * remove file
    
    * end turn route
    
    * responses
    
    * return message also
    
    * use cache for collectors
    
    * hintRespondingTo
    
    * incomingId
    
    * bump messaging client
    
    * turn id
    
    * collect flag
    
    * fix ui
    
    * backend validate user token is correct
    
    * schema
    
    * socket auth
    
    * update testing ui
    
    * bring changes
    
    * remove user token api
    
    * remove user token api
    
    * revert
    
    * remove correct user token api
    
    * bring changes
    
    * extract constant
    
    * verifyToken
    
    * fix tests
    
    * users.get
    
    * socket prevent lost requests
    
    * conversations socket
    
    * some refact and changes
    
    * refact schema validation
    
    * refact user rights check
    
    * remove nonsense
    
    * socket refact
    
    * move socket code at of api start
    
    * socket root class
    
    * remove private
    
    * refact schema
    
    * fix
    
    * fix
    
    * dont server client
    
    * move api routes to functions
    
    * different collect route
    
    * better api route names
    
    * refact schema validation
    
    * refact messages
    
    * refact conversations
    
    * refact users
    
    * remove ui from changes
    
    * update ui
    
    * custom timeout
    
    * fix
    
    * better socket
    
    * better socket
    
    * bring changes
    
    * fix
    
    * fix
    
    * tests start
    
    * fix
    
    * more tests
    
    * tests
    
    * pr comments
    
    * fix
    
    * fix
    
    * comments
    
    * fixes and tests
    
    * adjust api.rest
    
    * better tests
    
    * fix delete message
    
    * webchat -> chat for test packages
    
    * init package
    
    * ctrl+c ctrl+v
    
    * fix prettier
    
    * fix decorators
    
    * enable jsx
    
    * add dep to components package
    
    * fix deps
    
    * fix strict
    
    * comment out some code to compile
    
    * .d.ts -> .ts
    
    * dont use components package for now
    
    * fix
    
    * run in board
    
    * don't import css yet
    
    * show something
    
    * stop ui crash
    
    * display something
    
    * remove test message
    
    * todos
    
    * text communication
    
    * fix styling
    
    * fix receive own message
    
    * load creds
    
    * fix switching conversation
    
    * fix create conversation
    
    * little fix
    
    * fix typing indicators
    
    * fixes
    
    * remove css imports
    
    * fix css
    
    * fullscreen prop
    
    * comment old stuff
    
    * fullscreen
    
    * fixes
    
    * fix merge
    
    * remove tsconfig change
    
    * remove dep on components package
    
    * fix compile errors
    
    * fix merge
    
    * fix
    
    * inject package
    
    * fix
    
    * parcel v2
    
    * Revert "parcel v2"
    
    This reverts commit 1c273c14d974b6284200dbfbf96ec187a47dcf0d.
    
    * pr comments
    
    * revert some changes
    
    * fix
    
    * remove console.log
    
    * fix config
    
    * fix
    
    * fix
    
    * url -> messagingUrl
    
    * fix
    
    * fix
    
    * v0.0.2

---
+++ packages/inject/src/index.tsx
@@ -0,0 +1,6 @@
+import 'regenerator-runtime/runtime'
+import { ExposedWebChat } from '@botpress/webchat'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<ExposedWebChat fullscreen={false} />, document.getElementById('app'))
