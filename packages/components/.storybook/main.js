90b7d605d09e125e3dea1767674bab842272b855
Charles Catta <charles.catta@botpress.com>
2021-11-12 14:19:42 -0500

    Fix warning storybook

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -3,6 +3,9 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  features: {
+    postcss: false,
+  },
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
