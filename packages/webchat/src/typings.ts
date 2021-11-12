057a71ab1f2dfa4b3e638c8dbae079332611f2cb
Samuel Massé <59894025+samuelmasse@users.noreply.github.com>
2021-11-12 17:27:27 -0500

    chore(webchat): display something (#235)
    * user token start
    
    * tests
    
    * refactor auth
    
    * caching
    
    * api
    
    * fix test
    
    * comment
    
    * expiry
    
    * batching
    
    * remove user token implementation
    
    * Revert "remove user token implementation"
    
    This reverts commit 1e1dd7807671675231b9ac09b244928ede4dc179.
    
    * refact auth
    
    * remove file
    
    * fix
    
    * refact client auth function
    
    * refact function
    
    * refact
    
    * fix
    
    * remove file
    
    * end turn route
    
    * responses
    
    * return message also
    
    * use cache for collectors
    
    * hintRespondingTo
    
    * incomingId
    
    * bump messaging client
    
    * turn id
    
    * collect flag
    
    * fix ui
    
    * backend validate user token is correct
    
    * schema
    
    * socket auth
    
    * update testing ui
    
    * bring changes
    
    * remove user token api
    
    * remove user token api
    
    * revert
    
    * remove correct user token api
    
    * bring changes
    
    * extract constant
    
    * verifyToken
    
    * fix tests
    
    * users.get
    
    * socket prevent lost requests
    
    * conversations socket
    
    * some refact and changes
    
    * refact schema validation
    
    * refact user rights check
    
    * remove nonsense
    
    * socket refact
    
    * move socket code at of api start
    
    * socket root class
    
    * remove private
    
    * refact schema
    
    * fix
    
    * fix
    
    * dont server client
    
    * move api routes to functions
    
    * different collect route
    
    * better api route names
    
    * refact schema validation
    
    * refact messages
    
    * refact conversations
    
    * refact users
    
    * remove ui from changes
    
    * update ui
    
    * custom timeout
    
    * fix
    
    * better socket
    
    * better socket
    
    * bring changes
    
    * fix
    
    * fix
    
    * tests start
    
    * fix
    
    * more tests
    
    * tests
    
    * pr comments
    
    * fix
    
    * fix
    
    * comments
    
    * fixes and tests
    
    * adjust api.rest
    
    * better tests
    
    * fix delete message
    
    * webchat -> chat for test packages
    
    * init package
    
    * ctrl+c ctrl+v
    
    * fix prettier
    
    * fix decorators
    
    * enable jsx
    
    * add dep to components package
    
    * fix deps
    
    * fix strict
    
    * comment out some code to compile
    
    * .d.ts -> .ts
    
    * dont use components package for now
    
    * fix
    
    * run in board
    
    * don't import css yet
    
    * show something
    
    * stop ui crash
    
    * display something
    
    * remove test message
    
    * todos
    
    * text communication
    
    * fix styling
    
    * fix receive own message
    
    * load creds
    
    * fix switching conversation
    
    * fix create conversation
    
    * little fix
    
    * fix typing indicators
    
    * fixes
    
    * remove css imports
    
    * fix css
    
    * fullscreen prop
    
    * comment old stuff
    
    * fullscreen
    
    * fixes
    
    * fix merge
    
    * remove tsconfig change
    
    * remove dep on components package
    
    * fix compile errors
    
    * fix merge
    
    * fix
    
    * pr comments
    
    * revert some changes
    
    * fix
    
    * remove console.log
    
    * fix
    
    * url -> messagingUrl

--- packages/webchat/src/typings.ts
+++ packages/webchat/src/typings.ts
@@ -165,6 +165,10 @@ export interface StudioConnector {
 }
 
 export interface Config {
+  /** Url of the messaging server */
+  messagingUrl: string
+  /** Id of your messaging client */
+  clientId: string
   botId?: string
   externalAuthToken?: string
   userId?: string
@@ -172,9 +176,9 @@ export interface Config {
   /** Allows to set a different user id for different windows (eg: studio, specific bot, etc) */
   userIdScope?: string
   enableReset: boolean
-  stylesheet: string
+  stylesheet?: string
   isEmulator?: boolean
-  extraStylesheet: string
+  extraStylesheet?: string
   showConversationsButton: boolean
   showUserName: boolean
   showUserAvatar: boolean
@@ -215,7 +219,7 @@ export interface Config {
   /** Experimental: expose the store to the parent frame for more control on the webchat's behavior */
   exposeStore: boolean
   /** Reference ensures that a specific value and its signature are valid */
-  reference: string
+  reference?: string
   /** If true, Websocket is created when the Webchat is opened. Bot cannot be proactive. */
   lazySocket?: boolean
   /** If true, chat will no longer play the notification sound for new messages. */
