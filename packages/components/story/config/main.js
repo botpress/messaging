e02a1820c1505ca142cadc6bcc4e4ec781b7ecff
Samuel Massé <samuelmasse4@gmail.com>
2021-11-15 13:35:17 -0500

    move storybook config

---
+++ packages/components/story/config/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  features: {
+    postcss: false
+  },
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+}
--- packages/components/story/config/preview.js
+++ packages/components/story/config/preview.js
@@ -1,12 +1,12 @@
-import './botpress-default.css'
+import '../../css/botpress-default.css'
 import './custom.css'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 }
