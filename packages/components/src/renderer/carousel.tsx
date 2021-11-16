9509f69c7dd352456f57c0dd001ab00165f0e513
Samuel Massé <samuelmasse4@gmail.com>
2021-11-16 15:54:03 -0500

    fix

--- packages/components/src/renderer/carousel.tsx
+++ packages/components/src/renderer/carousel.tsx
@@ -2,8 +2,8 @@ import React, { createRef } from 'react'
 import Slider, { Settings } from 'react-slick'
 
 // Added those manually to remove the font dependencies which keeps showing 404 not found
-import '../../../css/slick-theme.css'
-import '../../../css/slick.css'
+import '../../css/slick-theme.css'
+import '../../css/slick.css'
 import { ActionButton, ActionType } from '../content-typings'
 import { MessageTypeHandlerProps } from '../typings'
 
