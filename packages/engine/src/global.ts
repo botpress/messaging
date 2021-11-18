5974c483829c6f4951cf98c96801de695520f9bb
Samuel Massé <samuelmasse4@gmail.com>
2021-11-18 16:58:15 -0500

    move global config

---
+++ packages/engine/src/global.ts
@@ -0,0 +1,37 @@
+export interface EngineEnv {
+  NODE_ENV?: 'production' | 'development'
+
+  // logger
+  LOGGING_ENABLED?: string
+  SINGLE_LINE_LOGGING?: string
+  DISABLE_LOGGING_TIMESTAMP?: string
+
+  // database
+  DATABASE_URL?: string
+  DATABASE_POOL?: string
+
+  // migration
+  AUTO_MIGRATE?: string
+  MIGRATE_TARGET?: string
+  MIGRATE_CMD?: string
+  MIGRATE_DRYRUN?: string
+  TESTMIG_DB_VERSION?: string
+
+  // crypto
+  ENCRYPTION_KEY?: string
+
+  // distributed
+  CLUSTER_ENABLED?: string
+  REDIS_URL?: string
+  REDIS_OPTIONS?: string
+  REDIS_SCOPE?: string
+
+  // batching
+  BATCHING_ENABLED?: string
+}
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends EngineEnv {}
+  }
+}
