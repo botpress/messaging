b592aa3962a3232d67e5f29bb3ae8e04f4e38a0a
Samuel Massé <samuelmasse4@gmail.com>
2021-11-18 13:09:51 -0500

    move table to engine

--- packages/server/src/database/service.ts
+++ packages/server/src/database/service.ts
@@ -1,8 +1,7 @@
-import { Service } from '@botpress/messaging-engine'
+import { Service, Table } from '@botpress/messaging-engine'
 import fs from 'fs'
 import knex, { Knex } from 'knex'
 import path from 'path'
-import { Table } from '../base/table'
 import { Logger } from '../logger/types'
 
 export class DatabaseService extends Service {
