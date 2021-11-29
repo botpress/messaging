dd38dbb7f0e3148b083e82533254d0d1ecae7704
Samuel Massé <samuelmasse4@gmail.com>
2021-11-29 13:10:12 -0500

    more tests

--- packages/server/test/functional/status.test.ts
+++ packages/server/test/functional/status.test.ts
@@ -1,4 +1,5 @@
 import crypto from 'crypto'
+import ms from 'ms'
 import { validate as validateUuid } from 'uuid'
 import { Conduit } from '../../src/conduits/types'
 import { Provider } from '../../src/providers/types'
@@ -8,7 +9,13 @@ import { app, setupApp } from './utils'
 
 describe('Status', () => {
   let status: StatusService
-  let state: { provider: Provider; conduit: Conduit; status?: ConduitStatus }
+  let state: {
+    provider: Provider
+    conduit: Conduit
+    conduit2: Conduit
+    status?: ConduitStatus
+    status2?: ConduitStatus
+  }
 
   beforeAll(async () => {
     await setupApp()
@@ -16,11 +23,14 @@ describe('Status', () => {
     const provider = await app.providers.create(crypto.randomBytes(20).toString('hex'), false)
     const channel = app.channels.getByName('twilio')
     const conduit = await app.conduits.create(provider.id, channel.id, { accountSID: 'abc', authToken: 'sds' })
+    const channel2 = app.channels.getByName('slack')
+    const conduit2 = await app.conduits.create(provider.id, channel2.id, { botToken: 'abc', signingSecret: 'sds' })
 
     status = app.status
     state = {
       provider,
-      conduit
+      conduit,
+      conduit2
     }
   })
 
@@ -52,5 +62,35 @@ describe('Status', () => {
     const st = await status.get(state.conduit.id)
 
     expect(st).toEqual(state.status)
+  })
+
+  test('List outdated should contain status', async () => {
+    const outdateds = await status.listOutdated(ms('1h'), 10, 10)
+
+    expect(outdateds).toEqual([state.status])
+  })
+
+  test('Create status again should fail', async () => {
+    await expect(status.create(state.conduit.id)).rejects.toThrow()
+  })
+
+  test('Create status for other conduit', async () => {
+    let st = await status.get(state.conduit2.id)
+    expect(st).toBeUndefined()
+
+    st = await status.create(state.conduit2.id)
+
+    expect(validateUuid(st.conduitId)).toBeTruthy()
+    expect(st.numberOfErrors).toBe(0)
+    expect(st.initializedOn).toBeUndefined()
+    expect(st.lastError).toBeUndefined()
+
+    state.status2 = st
+  })
+
+  test('List outdated should contain both status', async () => {
+    const outdateds = await status.listOutdated(ms('1h'), 10, 10)
+
+    expect(outdateds).toEqual([state.status, state.status2])
   })
 })
