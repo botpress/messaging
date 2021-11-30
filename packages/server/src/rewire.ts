37bc5c1212a1e34aff4158d6e8bf0019705ea02a
Samuel Massé <samuelmasse4@gmail.com>
2021-11-29 20:52:54 -0500

    fix rewire

--- packages/server/src/rewire.ts
+++ packages/server/src/rewire.ts
@@ -2,14 +2,14 @@ import Module from 'module'
 
 // Rewires imports to target the source code of other modules when transpiling
 if (process.env.TS_NODE_DEV) {
-  const originalRequire = Module.prototype.require
+  const originalRequire = Module.prototype.require as (id: string) => any
 
-  const rewire = function (this: NodeRequireFunction, mod: string) {
+  const rewire = function (this: (id: string) => any, mod: string) {
     if (mod.startsWith('@botpress')) {
       return originalRequire.apply(this, [mod + '/src'])
     }
     return originalRequire.apply(this, [mod])
   }
 
-  Module.prototype.require = rewire
+  Module.prototype.require = rewire as any
 }
