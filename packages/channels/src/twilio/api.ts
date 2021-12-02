8123e11a580a97b78620532dbed858604a20248d
Samuel Massé <samuelmasse4@gmail.com>
2021-12-02 14:31:23 -0500

    twilio validation

--- packages/channels/src/twilio/api.ts
+++ packages/channels/src/twilio/api.ts
@@ -1,4 +1,5 @@
 import express, { Response } from 'express'
+import { validateRequest } from 'twilio'
 import { ChannelApiManager, ChannelApiRequest } from '../base/api'
 import { TwilioService } from './service'
 
@@ -11,12 +12,20 @@ export class TwilioApi {
   }
 
   private async handleRequest(req: ChannelApiRequest, res: Response) {
-    const botPhoneNumber = req.body.To
-    const userPhoneNumber = req.body.From
-    const content = { type: 'text', text: req.body.Body }
+    const signature = req.headers['x-twilio-signature'] as string
+    const { config } = this.service.get(req.scope)
 
-    await this.service.receive(req.scope, { identity: botPhoneNumber, sender: userPhoneNumber, thread: '*' }, content)
+    // TODO: what to do with webhook url?
+    if (validateRequest(config.authToken, signature, (config as any).webhookUrl, req.body)) {
+      const botPhoneNumber = req.body.To
+      const userPhoneNumber = req.body.From
+      const content = { type: 'text', text: req.body.Body }
 
-    res.sendStatus(204)
+      await this.service.receive(req.scope, { identity: botPhoneNumber, sender: userPhoneNumber, thread: '*' }, content)
+
+      res.sendStatus(204)
+    } else {
+      res.sendStatus(401)
+    }
   }
 }
