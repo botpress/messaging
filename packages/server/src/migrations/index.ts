990282cbe7000107208e69cd759ae1c1a014eb54
Samuel Massé <samuelmasse4@gmail.com>
2021-12-03 14:33:03 -0500

    rename file

--- packages/server/src/migrations/index.ts
+++ packages/server/src/migrations/index.ts
@@ -1,4 +1,4 @@
 import { Migration } from '@botpress/messaging-engine'
-import { StatusMigration } from './0.1.18-status'
+import { StatusMigration } from './0.1.19-status'
 
 export const Migrations: { new (): Migration }[] = [StatusMigration]
