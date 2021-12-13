f92111c83a083c726d438921f7888cdd0bc93772
Samuel Massé <59894025+samuelmasse@users.noreply.github.com>
2021-12-13 18:51:15 -0500

    fix(sync): add lock for provider name (#284)
    * fix(sync): add lock for provider name
    
    * migration

---
+++ packages/server/src/migrations/0.1.20-fix-client-schema.ts
@@ -0,0 +1,25 @@
+import { Migration } from '@botpress/messaging-engine'
+
+export class FixClientSchemaMigration extends Migration {
+  meta = {
+    name: FixClientSchemaMigration.name,
+    description: 'Fixes broken msg_clients table schema',
+    version: '0.1.20'
+  }
+
+  async valid() {
+    return this.trx.schema.hasTable('msg_clients')
+  }
+
+  async applied() {
+    return false
+  }
+
+  async up() {
+    return this.trx.schema.alterTable('msg_clients', (table) => {
+      table.uuid('providerId').nullable().alter()
+    })
+  }
+
+  async down() {}
+}
--- packages/server/src/migrations/index.ts
+++ packages/server/src/migrations/index.ts
@@ -1,4 +1,5 @@
 import { Migration } from '@botpress/messaging-engine'
 import { StatusMigration } from './0.1.19-status'
+import { FixClientSchemaMigration } from './0.1.20-fix-client-schema'
 
-export const Migrations: { new (): Migration }[] = [StatusMigration]
+export const Migrations: { new (): Migration }[] = [StatusMigration, FixClientSchemaMigration]
