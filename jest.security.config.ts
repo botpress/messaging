ac2c4c643e903ba76a0da5cdaa773c9e1f1fa661
Samuel Massé <samuelmasse4@gmail.com>
2022-01-06 13:39:46 -0500

    fix

--- jest.security.config.ts
+++ jest.security.config.ts
@@ -1,7 +1,6 @@
 import ServerConfig from './packages/server/test/tsconfig.json'
 import type { Config } from '@jest/types'
 import { pathsToModuleNameMapper } from 'ts-jest'
-import { defaults as tsjPreset } from 'ts-jest/presets'
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
