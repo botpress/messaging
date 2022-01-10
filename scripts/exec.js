357694da702bfd4bd2dfe2d9a29ef908c49fb008
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-01-10 15:21:05 -0500

    should fix yarn release

--- scripts/exec.js
+++ scripts/exec.js
@@ -2,7 +2,7 @@ require('bluebird-global')
 const { exec } = require('child_process')
 
 const execute = async (cmd, opts, { silent } = { silent: false }) => {
-  await Promise.fromCallback(cb => {
+  await Promise.fromCallback((cb) => {
     const proc = exec(cmd, opts, cb)
     if (!silent) {
       proc.stdout.pipe(process.stdout)
