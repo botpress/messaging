2cae76a92cb691cfb1f93be5c59f0929a8c9f55f
Samuel Massé <samuelmasse4@gmail.com>
2022-01-13 20:01:08 -0500

    fix

--- packages/board/src/app.tsx
+++ packages/board/src/app.tsx
@@ -15,7 +15,7 @@ new BoardLinker(
 
 const config: Partial<Config> = {
   messagingUrl: 'http://localhost:3100',
-  clientId: '625c1eef-3f6a-4693-bd87-42802b1ff9d0'
+  clientId: 'c28082df-0582-4ace-94cf-e6e293593b63'
 }
 
 ReactDOM.render(<ExposedWebChat config={config as Config} fullscreen={true} />, document.getElementById('oldwebchat'))
