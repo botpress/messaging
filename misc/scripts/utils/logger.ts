9d642f6d74c881fad49fee09cd348213ef1056e8
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-01-18 15:10:43 -0500

    removed chalk in favor of cli-color

--- misc/scripts/utils/logger.ts
+++ misc/scripts/utils/logger.ts
@@ -1,9 +1,9 @@
-import chalk from 'chalk'
+import clc from 'cli-color'
 
 const logger = {
-  info: (msg: string) => console.info(`${chalk.green('[INFO]')} ${msg}`),
-  warning: (msg: string) => console.info(`${chalk.yellow('[WARNING]')} ${msg}`),
-  error: (msg: string, err?: Error | string) => console.info(`${chalk.red('[ERROR]')} ${msg}`, err)
+  info: (msg: string) => console.info(`${clc.green('[INFO]')} ${msg}`),
+  warning: (msg: string) => console.info(`${clc.yellow('[WARNING]')} ${msg}`),
+  error: (msg: string, err?: Error | string) => console.info(`${clc.red('[ERROR]')} ${msg}`, err)
 }
 
 export default logger
