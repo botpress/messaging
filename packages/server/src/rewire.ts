6a79debe782a16ba2e37043c849c3df0b1266b38
Samuel Massé <samuelmasse4@gmail.com>
2022-01-27 20:31:32 -0500

    rewire comment

--- packages/server/src/rewire.ts
+++ packages/server/src/rewire.ts
@@ -5,7 +5,12 @@ if (process.env.TS_NODE_DEV) {
   const originalRequire = Module.prototype.require as (id: string) => any
 
   const rewire = function (this: (id: string) => any, mod: string) {
-    if (mod.startsWith('@botpress/messaging') && !mod.endsWith('legacy')) {
+    if (
+      mod.startsWith('@botpress/messaging') &&
+      // we don't want to rewire the legacy channels since we get them from npm
+      // so we don't have the source code in the repo
+      mod !== '@botpress/messaging-channels-legacy'
+    ) {
       return originalRequire.apply(this, [mod + '/src'])
     }
     return originalRequire.apply(this, [mod])
