d6fa4bd26aea8bf36260eb702ad68a8b8241f17c
Samuel Massé <samuelmasse4@gmail.com>
2022-03-03 19:01:19 -0500

    tests

---
+++ packages/server/test/integration/provisions.test.ts
@@ -0,0 +1,78 @@
+import { Client } from '@botpress/framework'
+import { Provider } from '../../src/providers/types'
+import { ProvisionService } from '../../src/provisions/service'
+import { Provision } from '../../src/provisions/types'
+import { app, randStr, setupApp } from '../utils'
+
+describe('Provisions', () => {
+  let provisions: ProvisionService
+  let querySpy: jest.SpyInstance
+  let state: { client: Client; provider: Provider; provision?: Provision }
+
+  beforeAll(async () => {
+    await setupApp()
+    provisions = app.provisions
+    querySpy = jest.spyOn(provisions as any, 'query')
+
+    state = {
+      client: await app.clients.create(),
+      provider: await app.providers.create(randStr(), false)
+    }
+  })
+
+  afterAll(async () => {
+    await app.destroy()
+  })
+
+  beforeEach(async () => {
+    app.caching.resetAll()
+  })
+
+  test('Create provision', async () => {
+    const provision = await provisions.create(state.client.id, state.provider.id)
+
+    expect(provision).toBeDefined()
+    expect(provision.clientId).toEqual(state.client.id)
+    expect(provision.providerId).toEqual(state.provider.id)
+
+    state.provision = provision
+  })
+
+  test('Get provision by client id', async () => {
+    const provision = await provisions.getByClientId(state.client.id)
+    expect(provision).toEqual(state.provision)
+    expect(querySpy).toHaveBeenCalledTimes(1)
+  })
+
+  test('Get provision by client id cached', async () => {
+    const provision = await provisions.getByClientId(state.client.id)
+    expect(provision).toEqual(state.provision)
+    expect(querySpy).toHaveBeenCalledTimes(1)
+
+    for (let i = 0; i < 10; i++) {
+      const cached = await provisions.getByClientId(state.client.id)
+      expect(cached).toEqual(state.provision)
+    }
+
+    expect(querySpy).toHaveBeenCalledTimes(1)
+  })
+
+  test('Get provision by provider id', async () => {
+    const provision = await provisions.getByProviderId(state.provider.id)
+    expect(provision).toEqual(state.provision)
+    expect(querySpy).toHaveBeenCalledTimes(1)
+  })
+
+  test('Get provision by provider id cached', async () => {
+    const provision = await provisions.getByProviderId(state.provider.id)
+    expect(provision).toEqual(state.provision)
+    expect(querySpy).toHaveBeenCalledTimes(1)
+
+    for (let i = 0; i < 10; i++) {
+      const cached = await provisions.getByProviderId(state.provider.id)
+      expect(cached).toEqual(state.provision)
+    }
+
+    expect(querySpy).toHaveBeenCalledTimes(1)
+  })
+})
