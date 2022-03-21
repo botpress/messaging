7acac7ea561a7215feb968d1f30ca8079a84fcc4
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-03-21 19:29:05 -0400

    fix tests on postgresql

--- packages/server/test/migration/migrations-diff.test.ts
+++ packages/server/test/migration/migrations-diff.test.ts
@@ -7,6 +7,12 @@ import { startMessagingServer } from './utils/server'
 
 const TIMEOUT = 30000
 
+const sanitize = (str: string) => {
+  // Removes invalid characters for PostgreSQL table name
+  // (see: https://www.postgresql.org/docs/7/syntax525.htm)
+  return str.replace(/\./g, '_').replace(/\-/g, '_').slice(0, 32)
+}
+
 describe('Migrations diff tests', () => {
   const migrationFolder = path.join(__dirname, '../../src/migrations')
   const migrationFiles = fs.readdirSync(migrationFolder)
@@ -14,12 +20,12 @@ describe('Migrations diff tests', () => {
     .filter((f) => !isNaN(Number(f.charAt(0)))) // Only keep migration files
     .map((f) => {
       const version = f.split('-')[0]
-      return [f, version, decrement(version)]
+      return [f.replace('.ts', ''), version, decrement(version)]
     })
 
   describe.each(migrations)('%s', (migrationName, migrationVersion, previousVersion) => {
-    const afterMigrationDatabase = `after_mig_${migrationName}`
-    const beforeMigrationDatabase = `before_mig_${migrationName}`
+    const afterMigrationDatabase = sanitize(`after_mig_${migrationName}`)
+    const beforeMigrationDatabase = sanitize(`before_mig_${migrationName}`)
 
     test(
       'Should run each migrations up to the migration we are testing',
