4be84ffd0e92056bde2732e107a9fe5a16dd6348
Samuel Massé <samuelmasse4@gmail.com>
2022-03-31 20:32:50 -0400

    sort env

--- packages/framework/src/global.ts
+++ packages/framework/src/global.ts
@@ -1,6 +1,14 @@
 import { EngineEnv } from '@botpress/messaging-engine'
 
-export type FrameworkEnv = EngineEnv & {}
+export type FrameworkEnv = EngineEnv & {
+  PORT?: string
+  EXTERNAL_URL?: string
+  INTERNAL_PASSWORD?: string
+  ADMIN_KEY?: string
+  APM_ENABLED?: string
+  NO_LOGO?: string
+  SKIP_LOAD_ENV?: string
+}
 
 declare global {
   namespace NodeJS {
