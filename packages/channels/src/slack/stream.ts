472c346ffcc28c1e04c3ba4d42b8028c914c0c1b
Samuel Massé <samuelmasse4@gmail.com>
2022-04-05 14:56:48 -0400

    slack validation

--- packages/channels/src/slack/stream.ts
+++ packages/channels/src/slack/stream.ts
@@ -1,7 +1,9 @@
+import { ChannelTestError } from '..'
 import { ChannelContext } from '../base/context'
 import { CardToCarouselRenderer } from '../base/renderers/card'
 import { ChannelSender } from '../base/sender'
 import { TypingSender } from '../base/senders/typing'
+import { ChannelTestEvent } from '../base/service'
 import { ChannelStream } from '../base/stream'
 import { SlackContext } from './context'
 import { SlackRenderers } from './renderers'
@@ -15,6 +17,23 @@ export class SlackStream extends ChannelStream<SlackService, SlackContext> {
 
   get senders(): ChannelSender<ChannelContext<any>>[] {
     return [new TypingSender(), ...SlackSenders]
+  }
+
+  async setup() {
+    await super.setup()
+    this.service.on('test', this.handleTest.bind(this))
+  }
+
+  private async handleTest({ scope }: ChannelTestEvent) {
+    const { app, config } = this.service.get(scope)
+
+    try {
+      await app.client.auth.test()
+    } catch {
+      throw new ChannelTestError('unable to authenticate slack request with the provided bot token', 'slack', [
+        'botToken'
+      ])
+    }
   }
 
   protected async getContext(base: ChannelContext<any>): Promise<SlackContext> {
