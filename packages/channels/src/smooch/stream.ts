6307aee78fa93359d73a05f6cae80efe6c40313b
Samuel Massé <samuelmasse4@gmail.com>
2022-04-05 17:21:15 -0400

    smooch validation

--- packages/channels/src/smooch/stream.ts
+++ packages/channels/src/smooch/stream.ts
@@ -1,6 +1,8 @@
+import { ChannelTestError } from '../base/channel'
 import { ChannelContext } from '../base/context'
 import { CardToCarouselRenderer } from '../base/renderers/card'
 import { DropdownToChoicesRenderer } from '../base/renderers/dropdown'
+import { ChannelTestEvent } from '../base/service'
 import { ChannelStream } from '../base/stream'
 import { SmoochContext } from './context'
 import { SmoochRenderers } from './renderers'
@@ -14,6 +16,25 @@ export class SmoochStream extends ChannelStream<SmoochService, SmoochContext> {
 
   get senders() {
     return SmoochSenders
+  }
+
+  async setup() {
+    await super.setup()
+    this.service.on('test', this.handleTest.bind(this))
+  }
+
+  private async handleTest({ scope }: ChannelTestEvent) {
+    const { smooch, config } = this.service.get(scope)
+
+    try {
+      await smooch.apps.getApp(config.appId)
+    } catch {
+      throw new ChannelTestError(
+        'unable to reach smooch with the provided key id, key secret and app id combination',
+        'smooch',
+        'keySecret'
+      )
+    }
   }
 
   protected async getContext(base: ChannelContext<any>): Promise<SmoochContext> {
