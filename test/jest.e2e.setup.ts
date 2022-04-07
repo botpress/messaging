3eb0e5216a5be8ffb1572730e3d247768ad75402
Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>
2022-04-07 12:13:29 -0400

    chore(tests): use random unused port for e2e tests (#439)

--- test/jest.e2e.setup.ts
+++ test/jest.e2e.setup.ts
@@ -3,6 +3,7 @@ require('ts-node/register')
 
 import { setup as setupDevServer } from 'jest-dev-server'
 import path from 'path'
+import portfinder from 'portfinder'
 import { v4 as uuidv4 } from 'uuid'
 
 const setup = async () => {
@@ -14,24 +15,18 @@ const setup = async () => {
     process.env.DATABASE_TRANSIENT = 'true'
   }
 
+  const port = await portfinder.getPortPromise()
+  process.env.PORT = port.toString()
+
   await setupDevServer({
     command: 'yarn dev',
     launchTimeout: 30000,
     protocol: 'http',
     host: '127.0.0.1',
-    port: 3100,
+    port,
     path: 'status',
     usedPortAction: 'error'
   })
-}
-
-const randomLetters = (length: number) => {
-  const chars = 'abcdefghijklmnopqrstuvwxyz'
-  let str = ''
-  for (let i = 0; i < length; i++) {
-    str += chars.charAt(Math.floor(Math.random() * chars.length))
-  }
-  return str
 }
 
 export default setup
