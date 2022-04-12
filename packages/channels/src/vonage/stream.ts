3bc1b35de3f823bed7211deafbd95c941843d65a
Samuel Massé <samuelmasse4@gmail.com>
2022-04-12 10:50:20 -0400

    vonage

--- packages/channels/src/vonage/stream.ts
+++ packages/channels/src/vonage/stream.ts
@@ -1,6 +1,9 @@
+import axios from 'axios'
+import { ChannelTestError } from '../base/channel'
 import { ChannelContext } from '../base/context'
 import { CardToCarouselRenderer } from '../base/renderers/card'
 import { TypingSender } from '../base/senders/typing'
+import { ChannelTestEvent } from '../base/service'
 import { ChannelStream } from '../base/stream'
 import { VonageContext } from './context'
 import { VonageRenderers } from './renderers'
@@ -14,6 +17,27 @@ export class VonageStream extends ChannelStream<VonageService, VonageContext> {
 
   get senders() {
     return [new TypingSender(), ...VonageSenders]
+  }
+
+  async setup() {
+    await super.setup()
+    this.service.on('test', this.handleTest.bind(this))
+  }
+
+  private async handleTest({ scope }: ChannelTestEvent) {
+    const { config } = this.service.get(scope)
+
+    try {
+      await axios.get('https://api.nexmo.com/v2/applications', {
+        auth: { username: config.apiKey, password: config.apiSecret }
+      })
+    } catch (e) {
+      throw new ChannelTestError(
+        'unable to reach vonage using the provided api key and api secret',
+        'vonage',
+        'apiSecret'
+      )
+    }
   }
 
   protected async getContext(base: ChannelContext<any>): Promise<VonageContext> {
