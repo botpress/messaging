80a475f519fcc574f382e6b5686ef18187df664e
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-04-22 16:19:30 -0400

    disable logging by default

--- test/setup/database.ts
+++ test/setup/database.ts
@@ -3,10 +3,12 @@ import fs from 'fs'
 import path from 'path'
 import { v4 as uuidv4 } from 'uuid'
 
+const log = process.env.DEBUG === 'true'
+
 export const setupDatabase = async ({ postgresOnly }: { postgresOnly: boolean } = { postgresOnly: false }) => {
   if (process.env.POSTGRESQL === 'true') {
     try {
-      await compose.upAll({ cwd: path.join(__dirname), log: true })
+      await compose.upAll({ cwd: path.join(__dirname), log })
       process.env.DATABASE_URL = 'postgres://postgres:postgres@localhost:2345'
     } catch (e) {
       throw new Error(`An error occurred while trying to setup the PostgreSQL database: ${e}`)
@@ -19,7 +21,7 @@ export const setupDatabase = async ({ postgresOnly }: { postgresOnly: boolean } 
 export const teardownDatabase = async () => {
   if (process.env.POSTGRESQL === 'true') {
     try {
-      await compose.down({ cwd: path.join(__dirname), log: true })
+      await compose.down({ cwd: path.join(__dirname), log })
     } catch (e) {
       throw new Error(
         `An error occurred while trying to teardown the PostgreSQL database. 
