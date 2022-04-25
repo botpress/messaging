e39ff59cd8882f2987d6431676037bc7f2a56f3e
Samuel Massé <samuelmasse4@gmail.com>
2022-04-25 14:46:26 -0400

    remove pkg

--- misc/scripts/utils/version.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-
-export const getProjectVersion = () => {
-  return require(path.join(__dirname, '../../../package.json')).version
-}
-
-export const formatVersion = (version: string): string => {
-  return version.replace(/\./g, '_')
-}
