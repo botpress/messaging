9aa8bc02bc0de1dd74929cbc4c7e2d9e1d350a31
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-04-28 16:45:18 -0400

    remove console.log

--- packages/studio/server/src/studio/server.ts
+++ packages/studio/server/src/studio/server.ts
@@ -211,7 +211,6 @@ export class HTTPServer {
     })
 
     this.app.use(function handleUnexpectedError(err: any, req: any, res: any, next: any) {
-      console.log('err', err)
       const statusCode = err.statusCode || 400
       const errorCode = err.errorCode
       const message = err.message || err || 'Unexpected error'
