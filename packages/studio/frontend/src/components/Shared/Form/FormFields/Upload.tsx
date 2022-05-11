3d0d0e5c1d0a61ed89501899fb32d734568074e1
Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>
2022-05-11 15:13:20 -0400

    chore(studio): adapt media service to new conversation studio (#470)
    * chore(studio): adapt media service to new conversation studio
    
    * pr comment

--- packages/studio/frontend/src/components/Shared/Form/FormFields/Upload.tsx
+++ packages/studio/frontend/src/components/Shared/Form/FormFields/Upload.tsx
@@ -1,68 +1,35 @@
 import { FileInput, Icon } from '@blueprintjs/core'
-import React, { FC, Fragment, useReducer } from 'react'
+import React, { FC, Fragment, useState } from 'react'
 
 import * as sharedStyle from '../../../Shared/style.module.scss'
 import FileDisplay from '../../FileDisplay'
 import { UploadFieldProps } from './typings'
 
 const Upload: FC<UploadFieldProps> = (props) => {
-  const uploadReducer = (state, action) => {
-    if (action.type === 'uploadStart') {
-      return {
-        ...state,
-        error: null
-      }
-    } else if (action.type === 'deleteFile') {
-      props.onChange?.(undefined)
-      return {
-        ...state,
-        error: null
-      }
-    } else if (action.type === 'uploadError') {
-      const { error } = action.data
-
-      return {
-        ...state,
-        error
-      }
-    } else if (action.type === 'uploadSuccess') {
-      const { url } = action.data
-
-      props.onChange?.(url)
-      return {
-        ...state,
-        error: null
-      }
-    } else {
-      throw new Error("That action type isn't supported.")
-    }
-  }
-
-  const [state, dispatch] = useReducer(uploadReducer, {
-    error: null
-  })
-
-  const { error } = state
+  const [error, setError] = useState<string>()
 
   const deleteFile = () => {
-    dispatch({ type: 'deleteFile' })
+    setError(undefined)
+    props.onChange?.(undefined)
   }
 
-  const startUpload = async (event) => {
+  const startUpload = async (event: React.FormEvent<HTMLInputElement>) => {
+    setError(undefined)
+
     const data = new FormData()
-    data.append('file', event.target.files[0])
+    data.append('file', (event.target as HTMLInputElement).files[0])
 
-    dispatch({ type: 'uploadStart' })
-    await props.axios
-      .post(props.customPath ? props.customPath : 'media', data, { headers: { 'Content-Type': 'multipart/form-data' } })
-      .then((response) => {
-        const url: string = response.data.url
+    try {
+      const {
+        data: { url }
+      } = await props.axios.post<{ url: string }>(props.customPath ? props.customPath : 'media', data, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      })
 
-        dispatch({ type: 'uploadSuccess', data: { url } })
-      })
-      .catch((e) => {
-        dispatch({ type: 'uploadError', data: { error: e.message } })
-      })
+      props.onChange?.(url)
+    } catch (err) {
+      setError((err as Error).message)
+    }
   }
 
   const { value, type, filter } = props
@@ -74,7 +41,7 @@ const Upload: FC<UploadFieldProps> = (props) => {
       // e.g. video/*, audio/*, ...
       return `${type}/*`
     } else {
-      ;('*')
+      return '*'
     }
   }
 
