9f9501dc6bc9921d2535b443101698546cd2d0f9
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-05-20 09:56:20 -0400

    pr comment

--- packages/studio/frontend2/src/App.tsx
+++ packages/studio/frontend2/src/App.tsx
@@ -1,7 +1,7 @@
-//import { lang } from './components/Shared/translations'
 import React from 'react'
 import { useRoutes } from 'react-router'
 
+// We initialize the translation system here
 import './translations'
 import routes from './views'
 
