75d2e82fa87827066f1da894e5e19d7bc6d9b898
Samuel Massé <samuelmasse4@gmail.com>
2022-05-30 16:50:57 -0400

    list elements filters

--- packages/studio/server2/src/cms/api.ts
+++ packages/studio/server2/src/cms/api.ts
@@ -8,8 +8,7 @@ export class CmsApi {
 
   setup(router: PublicApiManager) {
     router.get('/cms/types', Schema.Api.ListTypes, this.listTypes.bind(this))
-    // TODO: why is this POST??
-    router.post('/cms/elements', Schema.Api.ListElements, this.listElements.bind(this))
+    router.post('/cms/:contentType?/elements', Schema.Api.ListElements, this.listElements.bind(this))
   }
 
   async listTypes(req: Request, res: Response) {
@@ -17,9 +16,26 @@ export class CmsApi {
   }
 
   async listElements(req: Request, res: Response) {
-    const ids = req.body.ids as string[] | undefined
+    const { contentType } = req.params
+    const { searchTerm, from, count, ids } = req.body
 
     let elements = await this.cms.listElements()
+
+    if (contentType) {
+      elements = elements.filter((x) => x.type === contentType)
+    }
+
+    if (searchTerm) {
+      // TODO: does not scale very well
+      elements = elements.filter((x) => x.id.includes(searchTerm) || JSON.stringify(x.formData).includes(searchTerm))
+    }
+
+    if (from) {
+      elements = elements.slice(from)
+    }
+
+    elements = elements.slice(0, count || 50)
+
     if (ids) {
       elements = elements.filter((x) => ids.includes)
     }
--- packages/studio/server2/src/cms/schema.ts
+++ packages/studio/server2/src/cms/schema.ts
@@ -3,7 +3,18 @@ import Joi from 'joi'
 
 const Api = {
   ListTypes: ReqSchema(),
-  ListElements: ReqSchema({ body: { ids: Joi.array().items(Joi.string()).optional() } })
+  ListElements: ReqSchema({
+    params: {
+      contentType: Joi.string()
+    },
+    body: {
+      count: Joi.number(),
+      from: Joi.number(),
+      searchTerm: Joi.string().allow(''),
+      sortOrder: Joi.array(),
+      ids: Joi.array().items(Joi.string()).optional()
+    }
+  })
 }
 
 export const Schema = { Api }
