a727489f524d4d486ddb25153e7708c6a9bdd267
Samuel Massé <samuelmasse4@gmail.com>
2022-05-30 17:58:10 -0400

    element usage

--- packages/studio/server2/src/qna/api.ts
+++ packages/studio/server2/src/qna/api.ts
@@ -8,10 +8,16 @@ export class QnaApi {
 
   setup(router: PublicApiManager) {
     router.get('/qna/questions', Schema.Api.List, this.list.bind(this))
+    // TODO: don't know what this is supposed to do
+    router.get('/qna/contentElementUsage', Schema.Api.Usage, this.usage.bind(this))
   }
 
   async list(req: Request, res: Response) {
     const qnas = await this.qnas.list()
     res.send({ items: qnas, count: qnas.length })
+  }
+
+  async usage(req: Request, res: Response) {
+    res.send({})
   }
 }
--- packages/studio/server2/src/qna/schema.ts
+++ packages/studio/server2/src/qna/schema.ts
@@ -2,7 +2,8 @@ import { ReqSchema } from '@botpress/framework'
 import Joi from 'joi'
 
 const Api = {
-  List: ReqSchema({ query: { limit: Joi.number(), offset: Joi.number(), question: Joi.string().allow('') } })
+  List: ReqSchema({ query: { limit: Joi.number(), offset: Joi.number(), question: Joi.string().allow('') } }),
+  Usage: ReqSchema()
 }
 
 export const Schema = { Api }
