9457e72b1048d324dbe74382216171eeea568ad6
michaelmass <10071388+michaelmass@users.noreply.github.com>
2022-06-09 17:13:12 -0400

    update

--- packages/server/src/tracing/trace.ts
+++ packages/server/src/tracing/trace.ts
@@ -1,19 +1,19 @@
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api'
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { JaegerExporter } from '@opentelemetry/exporter-jaeger'
 import { registerInstrumentations } from '@opentelemetry/instrumentation'
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { JaegerPropagator } from '@opentelemetry/propagator-jaeger'
 import { defaultServiceName, Resource } from '@opentelemetry/resources'
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api'
 import yn from 'yn'
 
 export const isEnabled = () => yn(process.env.TRACING_ENABLED, { default: false })
 
 const isDebugEnabled = () => yn(process.env.TRACING_DEBUG, { default: false })
 
-/** 
+/**
  * Warning: This function must be sync to instrument other packages
  * since it creates hooks in other packages it must be the first to execute
  */
@@ -25,7 +25,7 @@ export const init = () => {
   }
 
   if (isDebugEnabled()) {
-    diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+    diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG)
   }
 
   const propagator = new JaegerPropagator()
@@ -35,7 +35,7 @@ export const init = () => {
   const provider = new NodeTracerProvider({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: process.env.OTEL_SERVICE_NAME ?? defaultServiceName()
-    }),
+    })
   })
 
   provider.addSpanProcessor(processor)
@@ -43,6 +43,6 @@ export const init = () => {
 
   registerInstrumentations({
     tracerProvider: provider,
-    instrumentations: [getNodeAutoInstrumentations()],
+    instrumentations: [getNodeAutoInstrumentations()]
   })
 }
