e47f19d5b163cfc02aa6bd5bf8a2ce762cb6fc3c
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-08-09 16:33:04 -0400

    remove console.log

--- packages/webchat/src/utils/webchatEvents.ts
+++ packages/webchat/src/utils/webchatEvents.ts
@@ -1,5 +1,3 @@
 export const postMessageToParent = (type: string, value: any, chatId?: string) => {
-  console.log('value', value)
-  console.log('data', { type, value, chatId })
   window.parent?.postMessage({ type, value, chatId }, '*')
 }
