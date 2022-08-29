2b204c33e85a82508365ec8b598464508e995d00
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2022-08-29 17:30:55 -0400

    feat(webchat): create conversation from iframe api

--- packages/webchat/src/utils/webchatEvents.ts
+++ packages/webchat/src/utils/webchatEvents.ts
@@ -1,6 +1,9 @@
+import { cloneDeep } from 'lodash'
 import { WebchatEvent, WebchatEventType } from '../typings'
 
 export const postMessageToParent = (type: WebchatEventType, value: any, chatId: string) => {
+  //necessary because of potentially nested mobx proxy object isn't serializable
+  value = cloneDeep(value)
   const evt: WebchatEvent = { type, value, chatId }
   window.parent?.postMessage(evt, '*')
 }
