8da88cf214958fdcf429cbbcde06e42d5310538d
Sylvain Perron <sylvain.perron@botpress.com>
2023-09-11 14:18:04 -0400

    fix(webchat): typing indicators on msg sent

--- packages/webchat/src/store/index.ts
+++ packages/webchat/src/store/index.ts
@@ -338,6 +338,7 @@ class RootStore {
 
     this.composer.updateMessage('')
     try {
+      this._startTypingTimer()
       const message = await this.sendData({ type: 'text', text: textMessage })
       trackMessage('sent')
       if (message) {
