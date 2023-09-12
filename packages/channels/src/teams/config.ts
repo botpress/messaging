c480db059bb6f8a57ea674ba43384d02ae490241
sebburon <Sebastien.buron@botpress.com>
2023-09-12 09:37:00 -0400

    feat(messaging): single tenant option (#615)
    * feat(messaging): single tenant option
    
    * build messaging
    
    * build bin
    
    * sqlite
    
    * revert
    
    * Update packages/channels/src/teams/stream.ts
    
    Co-authored-by: Francois-Xavier P. Darveau <955524+EFF@users.noreply.github.com>
    
    * fix prettier
    
    ---------
    
    Co-authored-by: allardy <yann.allard1@gmail.com>
    Co-authored-by: David Vitor Antonio <david.vitora@gmail.com>
    Co-authored-by: Francois-Xavier P. Darveau <955524+EFF@users.noreply.github.com>

--- packages/channels/src/teams/config.ts
+++ packages/channels/src/teams/config.ts
@@ -4,9 +4,11 @@ import { ChannelConfig } from '../base/config'
 export interface TeamsConfig extends ChannelConfig {
   appId: string
   appPassword: string
+  tenantId: string
 }
 
 export const TeamsConfigSchema = {
   appId: Joi.string().required(),
-  appPassword: Joi.string().required()
+  appPassword: Joi.string().required(),
+  tenantId: Joi.string().optional()
 }
--- packages/channels/src/teams/stream.ts
+++ packages/channels/src/teams/stream.ts
@@ -32,9 +32,11 @@ export class TeamsStream extends ChannelStream<TeamsService, TeamsContext> {
         grant_type: 'client_credentials',
         client_id: config.appId,
         client_secret: config.appPassword,
+        tenant_id: config.tenantId,
         scope: 'https://api.botframework.com/.default'
       })
-      await axios.post('https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token', params.toString())
+      const tenant = config.tenantId ?? 'botframework.com'
+      await axios.post(`https://login.microsoftonline.com/${tenant}/oauth2/v2.0/token`, params.toString())
     } catch (e) {
       throw new ChannelTestError(
         'unable to reach teams using the provided app id and app password combination',
