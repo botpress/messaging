463be9f3061e4306d9af25eeb0a39495092717d5
David Vitor Antonio <david.vitora@gmail.com>
2024-01-04 18:22:07 -0300

    feat(teams): send attachments to core

--- packages/channels/src/base/api.ts
+++ packages/channels/src/base/api.ts
@@ -12,6 +12,18 @@ export class ChannelApi<TService extends ChannelService<any, any>> {
   makeUrl(callback: (scope: string) => Promise<string>) {
     this.urlCallback = callback
   }
+
+  mapMimeTypeToStandardType(mimeType: string) {
+    if (mimeType?.startsWith('image/')) {
+      return 'image'
+    } else if (mimeType?.startsWith('video/')) {
+      return 'video'
+    } else if (mimeType?.startsWith('audio/')) {
+      return 'audio'
+    } else {
+      return 'file'
+    }
+  }
 }
 
 export type Middleware<T> = (req: T, res: Response, next: NextFunction) => Promise<any>
