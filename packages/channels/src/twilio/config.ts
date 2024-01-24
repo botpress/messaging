f12fc4b2edf76ad57c18e80c4f619d4dddd7faa1
David Vitor Antonio <david.vitora@gmail.com>
2024-01-24 10:15:10 -0300

    feat(twillio): add message delay config (#649)
    * fix(engine): lock sqlite version
    
    * feat(twillio): add message delay config

--- packages/channels/src/twilio/config.ts
+++ packages/channels/src/twilio/config.ts
@@ -4,9 +4,11 @@ import { ChannelConfig } from '../base/config'
 export interface TwilioConfig extends ChannelConfig {
   accountSID: string
   authToken: string
+  messageDelay?: string
 }
 
 export const TwilioConfigSchema = {
   accountSID: Joi.string().regex(/^AC.*/).required(),
-  authToken: Joi.string().required()
+  authToken: Joi.string().required(),
+  messageDelay: Joi.string().optional()
 }
--- packages/channels/src/twilio/senders/common.ts
+++ packages/channels/src/twilio/senders/common.ts
@@ -1,3 +1,4 @@
+import ms from 'ms'
 import { CommonSender } from '../../base/senders/common'
 import { TwilioContext } from '../context'
 
@@ -9,6 +10,13 @@ export class TwilioCommonSender extends CommonSender {
         from: context.identity,
         to: context.sender
       })
+
+      const { messageDelay } = context.state.config
+      if (messageDelay) {
+        // depending on the account it might be required to limit the rps to each number
+        // usually this is an issue for carousels
+        await new Promise((resolve) => setTimeout(resolve, ms(messageDelay)))
+      }
     }
   }
 }
