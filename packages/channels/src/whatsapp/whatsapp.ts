843e6cab80414a92a5ee86669e54bc52930714e8
Varun Seth <varun.seth@ril.com>
2024-08-28 02:00:50 +0530

    added whatsapp interfaces for media and interactive messages

--- packages/channels/src/whatsapp/whatsapp.ts
+++ packages/channels/src/whatsapp/whatsapp.ts
@@ -10,6 +10,13 @@ export interface WhatsappPhoneNumberInfo {
   id: string
 }
 
+export interface WhatsappContact {
+  profile: {
+    name: string
+  }
+  wa_id: string
+}
+
 export interface WhatsappPayload {
   object: string
   entry: WhatsappEntry[]
@@ -20,27 +27,7 @@ export interface WhatsappEntry {
   changes: WhatsappChange[]
 }
 
-export interface WhatsappChange {
-  value: {
-    messaging_product: string
-    metadata: {
-      display_phone_number: string
-      phone_number_id: string
-    }
-    contacts: WhatsappContact[]
-    messages?: WhatsappMessage[]
-  }
-  field: string
-}
-
-export interface WhatsappContact {
-  profile: {
-    name: string
-  }
-  wa_id: string
-}
-
-export interface WhatsappMessage {
+export interface WhatsappIncomingMessage {
   from: string
   id: string
   timestamp: string
@@ -60,4 +47,85 @@ export interface WhatsappMessage {
       title: string
     }
   }
+}
+
+export interface WhatsappChange {
+  value: {
+    messaging_product: string
+    metadata: {
+      display_phone_number: string
+      phone_number_id: string
+    }
+    contacts: WhatsappContact[]
+    messages?: WhatsappIncomingMessage[]
+  }
+  field: string
+}
+
+
+export interface WhatsappText {
+  preview_url?: boolean
+  body: string
+}
+
+export interface WhatsappMedia {
+  link: string
+  caption?: string
+}
+
+export interface WhatsappLocation {
+  longitude: number
+  latitude: number
+  name?: string
+  address?: string
+}
+
+export interface WhatsappButton {
+  type: 'reply'
+  reply: {
+    id: string
+    title: string
+  }
+}
+
+export interface WhatsappRow {
+  id: string
+  title: string
+}
+
+export interface WhatsappSection {
+  rows: WhatsappRow[]
+}
+
+export interface WhatsappInteractive {
+  type: 'button' | 'list'
+  header?: {
+    type: 'text' | 'image' | 'video' | 'document'
+    text?: string
+    image?: WhatsappMedia
+    video?: WhatsappMedia
+    document?: WhatsappMedia
+  }
+  body?: {
+    text: string
+  }
+  footer?: {
+    text: string
+  }
+  action: {
+    button?: string
+    buttons?: WhatsappButton[]
+    sections?: WhatsappSection[]
+  }
+}
+
+export interface WhatsappOutgoingMessage {
+  type: 'text' | 'image' | 'audio' | 'video' | 'document' | 'location' | 'interactive'
+  text?: WhatsappText
+  image?: WhatsappMedia
+  audio?: WhatsappMedia
+  video?: WhatsappMedia
+  document?: WhatsappMedia
+  location?: WhatsappLocation
+  interactive?: WhatsappInteractive
 }
