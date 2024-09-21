0282a8b6d58f65492317c7698f16e2269edc1403
Varun Seth <varunseth2303@gmail.com>
2024-09-21 18:48:04 +0530

    using whatsapp interactice reply buttons and list messages if possible else using fallback logic.

--- packages/channels/src/whatsapp/renderers/carousel.ts
+++ packages/channels/src/whatsapp/renderers/carousel.ts
@@ -1,14 +1,20 @@
+
+import { IndexChoiceOption, IndexChoiceType } from '../../base/context'
 import { CarouselRenderer, CarouselContext } from '../../base/renderers/carousel'
 import { ActionOpenURL, ActionPostback, ActionSaySomething, CardContent, CarouselContent } from '../../content/types'
 import { WhatsappContext } from '../context'
-import { WhatsappButton, WhatsappOutgoingMessage } from '../whatsapp'
+import { WhatsappButton } from '../whatsapp'
 
 type Context = CarouselContext<WhatsappContext> & {
   buttons: WhatsappButton[]
+  options: IndexChoiceOption[]
+  index: number
 }
 
 export class WhatsappCarouselRenderer extends CarouselRenderer {
-  startRender(context: Context, carousel: CarouselContent) {}
+  startRender(context: Context, carousel: CarouselContent) {
+    context.options = []
+  }
 
   startRenderCard(context: Context, card: CardContent) {
     context.buttons = []
@@ -18,7 +24,7 @@ export class WhatsappCarouselRenderer extends CarouselRenderer {
     context.buttons.push({
       type: 'reply',
       reply: {
-        id: `open_url::${button.url}`,
+        id: `${IndexChoiceType.OpenUrl}::${button.url}`.substring(0, 256),
         title: button.title.substring(0, 20)
       }
     })
@@ -28,7 +34,7 @@ export class WhatsappCarouselRenderer extends CarouselRenderer {
     context.buttons.push({
       type: 'reply',
       reply: {
-        id: `postback::${button.payload}`,
+        id: `${IndexChoiceType.PostBack}::${button.payload}`.substring(0, 256),
         title: button.title.substring(0, 20)
       }
     })
@@ -38,22 +44,22 @@ export class WhatsappCarouselRenderer extends CarouselRenderer {
     context.buttons.push({
       type: 'reply',
       reply: {
-        id: `say_something::${button.text}`,
+        id: `${IndexChoiceType.SaySomething}::${button.text}`.substring(0, 256),
         title: button.title.substring(0, 20)
       }
     })
   }
 
   endRenderCard(context: Context, card: CardContent) {
-    let message: WhatsappOutgoingMessage
-
+    let message: any
     if (!context.buttons.length) {
+      const text = card.subtitle ? `${card.title}\n\n${card.subtitle}` : card.title
       if (card.image) {
         message = {
           type: 'image',
           image: {
             link: card.image,
-            caption: card.subtitle ? `${card.title}\n\n${card.subtitle}` : card.title
+            caption: text.substring(0, 1024)
           }
         }
       } else {
@@ -61,64 +67,65 @@ export class WhatsappCarouselRenderer extends CarouselRenderer {
           type: 'text',
           text: {
             preview_url: true,
-            body: card.subtitle ? `${card.title}\n\n${card.subtitle}` : card.title
+            body: text.substring(0, 4096)
           }
         }
       }
-    } else if (1 <= context.buttons.length && context.buttons.length <= 10) {
-      if (context.buttons.length <= 3) {
-        message = {
-          type: 'interactive',
-          interactive: {
-            type: 'button',
-            body: {
-              text: card.title.substring(0, 1024)
-            },
-            action: {
-              buttons: context.buttons
-            }
-          }
-        }
-      } else {
-        message = {
-          type: 'interactive',
-          interactive: {
-            type: 'list',
-            body: {
-              text: card.title.substring(0, 1024)
-            },
-            action: {
-              button: 'Select...',
-              sections: [
-                {
-                  rows: context.buttons.map((button) => ({
-                    id: button.reply.id,
-                    title: button.reply.title
-                  }))
-                }
-              ]
-            }
+    } else if (context.buttons.length <= 3) {
+      message = {
+        type: 'interactive',
+        interactive: {
+          type: 'button',
+          body: {
+            text: card.title.substring(0, 1024)
+          },
+          action: {
+            buttons: context.buttons
           }
         }
       }
-      if (message.interactive) {
-        if (card.image) {
-          message.interactive.header = {
-            type: 'image',
-            image: {
-              link: card.image
-            }
+      if (card.image) {
+        message.interactive.header = {
+          type: 'image',
+          image: {
+            link: card.image
           }
         }
-        if (card.subtitle) {
-          message.interactive.footer = {
-            text: card.subtitle.substring(0, 60)
+      }
+      if (card.subtitle) {
+        message.interactive.footer = {
+          text: card.subtitle.substring(0, 60)
+        }
+      }
+    } else if (!card.image && context.buttons.length <= 10) {
+      message = {
+        type: 'interactive',
+        interactive: {
+          type: 'list',
+          body: {
+            text: card.title.substring(0, 4096)
+          },
+          action: {
+            button: 'Select...',
+            sections: [
+              {
+                rows: context.buttons.map((button) => ({
+                  id: button.reply.id.substring(0, 200),
+                  title: button.reply.title.substring(0, 24)
+                }))
+              }
+            ]
           }
+        }
+      }
+      if (card.subtitle) {
+        message.interactive.footer = {
+          text: card.subtitle.substring(0, 60)
         }
       }
     } else {
-      const text = `*${card.title}*\n\n${card.subtitle ? `${card.subtitle}\n\n` : ''}
-        ${context.buttons.map(({ reply }, index) => `*${index + 1}.)* ${reply.title}`)
+      const text = `${card.title}\n\n${card.subtitle || ''}\n\n${context.buttons
+        .map(({ reply }, index) => `${index + context.options.length + 1}. ${reply.title}`)
         .join('\n')}`
 
       if (card.image) {
@@ -126,7 +133,7 @@ export class WhatsappCarouselRenderer extends CarouselRenderer {
           type: 'image',
           image: {
             link: card.image,
-            caption: text
+            caption: text.substring(0, 1024)
           }
         }
       } else {
@@ -134,13 +141,30 @@ export class WhatsappCarouselRenderer extends CarouselRenderer {
           type: 'text',
           text: {
             preview_url: false,
-            body: text
+            body: text.substring(0, 4096)
           }
         }
       }
+      context.options.push(...context.buttons.map((button) => {
+        const [type, value] = button.reply.id.split('::') as [IndexChoiceType, string]
+        return {
+          type: type === IndexChoiceType.OpenUrl ? IndexChoiceType.SaySomething : type,
+          title: button.reply.title,
+          value
+        }
+      }))
     }
     context.channel.messages.push(message)
   }
 
-  endRender(context: Context, carousel: CarouselContent) {}
+  endRender(context: Context, carousel: CarouselContent) {
+    if (context.options.length) {
+      context.channel.prepareIndexResponse(
+        context.channel.scope,
+        context.channel.identity,
+        context.channel.sender,
+        context.options
+      )
+    }
+  }
 }
